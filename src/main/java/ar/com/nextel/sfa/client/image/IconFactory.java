6e2864dcf5de985c4de80ce0742f71a71c7acc44
jlgperez <jlgperez>
2009-04-15 17:42:00 +0000

    se revierte cambio en iconFactory

--- src/main/java/ar/com/nextel/sfa/client/image/IconFactory.java
+++ src/main/java/ar/com/nextel/sfa/client/image/IconFactory.java
@@ -1,11 +1,7 @@
 package ar.com.nextel.sfa.client.image;
 
-import ar.com.nextel.sfa.client.widget.UILoader;
-
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.Image;
-import com.google.gwt.user.client.ui.Widget;
 
 public class IconFactory {
 
@@ -33,18 +29,5 @@ public class IconFactory {
 	
 	public static Image locked() {
 		return getIconBundle().locked().createImage();
-	}
-	
-	public static Image addImageWithListener(String imagen){
-		if (imagen.toString() == "lapiz"){
-			imagenFinal = lapiz();
-			imagenFinal.addClickListener(new ClickListener() {
-				public void onClick(Widget sender) {
-				//TODO: Modificar esta implementacion. Deberia llamar al servicio especifico que luego abrira la pantalla correspondiente.
-					UILoader.getInstance().setPage(UILoader.BUSCAR_CUENTA);
-				}
-			});			
-		}
-	return imagenFinal;
 	}
 }
