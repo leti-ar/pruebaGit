ca850663511b18bbd0d2dd825a1a7f616d81f1d7
jlgperez <jlgperez>
2009-04-15 20:33:02 +0000

    refactor name. Se cambian clases del modelo por Dto

---
+++ src/main/java/ar/com/nextel/sfa/client/dto/SolicitudServicioDto.java
@@ -0,0 +1,54 @@
+package ar.com.nextel.sfa.client.dto;
+
+import java.util.Date;
+
+import ar.com.nextel.model.solicitudes.beans.TipoAnticipo;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+public class SolicitudServicioDto implements IsSerializable {
+
+	private Long id;
+	private VendedorDto vendedor;
+	private GrupoSolicitudDto grupoSolicitud;
+	private CuentaDto cuenta;
+	private String numero;
+	private String numeroFlota;
+
+	// private CuentaPotencial cuentaPotencial;
+	// private OrigenSolicitud origen;
+	// private Vendedor vendedorDAE;
+
+	private DomicilioDto domicilioFacturacion;
+	private DomicilioDto domicilioEnvio;
+	private String aclaracionEntrega;
+	private String email;
+	private String emailLicencias;
+
+	private Double montoCreditoFideliacion;
+	private String fechaVencimientoCreditoFidelizacion;
+	private String montoDisponible;
+	private Long channelCode;
+	private Double pataconex;
+
+	private Boolean firmar = Boolean.FALSE;
+	// private EstadoAprobacionSolicitud estadoActual;
+	private String observaciones;
+
+	private TipoAnticipo tipoAnticipo;
+
+	// private Set<LineaSolicitudServicio> lineas = new HashSet<LineaSolicitudServicio>();
+
+	private Boolean pinValidationSuccess;
+	private Boolean scoringValidationSuccess;
+	private Long idVantive;
+
+	private Boolean vencimientoProcesado;
+
+	private Boolean generada;
+
+	private Boolean enCarga;
+
+	private Date fechaCreacion;
+
+}
