843601e00a51bb6514208ced825c565a9eb389e4
jlgperez <jlgperez>
2009-04-22 14:53:27 +0000

    se pasa logica de negocio a una clase transaccionable

--- src/main/java/ar/com/nextel/sfa/server/SolicitudRpcServiceImpl.java
+++ src/main/java/ar/com/nextel/sfa/server/SolicitudRpcServiceImpl.java
@@ -2,6 +2,8 @@ package ar.com.nextel.sfa.server;
 
 import javax.servlet.ServletException;
 
+import org.apache.commons.collections.Closure;
+import org.quartz.impl.jdbcjobstore.CloudscapeDelegate;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
@@ -11,85 +13,58 @@ import ar.com.nextel.business.legacy.financial.exception.FinancialSystemExceptio
 import ar.com.nextel.business.solicitudes.creation.SolicitudServicioBusinessOperator;
 import ar.com.nextel.business.solicitudes.creation.request.SolicitudServicioRequest;
 import ar.com.nextel.business.solicitudes.provider.SolicitudServicioProviderResult;
+import ar.com.nextel.business.vendedores.RegistroVendedores;
 import ar.com.nextel.components.accessMode.AccessAuthorization;
-import ar.com.nextel.components.accessMode.AccessPermission;
+import ar.com.nextel.framework.repository.Repository;
+import ar.com.nextel.framework.security.Usuario;
 import ar.com.nextel.model.cuentas.beans.EstadoCreditoFidelizacion;
+import ar.com.nextel.model.cuentas.beans.Vendedor;
 import ar.com.nextel.model.solicitudes.beans.SolicitudServicio;
+import ar.com.nextel.services.components.sessionContext.SessionContextLoader;
 import ar.com.nextel.services.exceptions.BusinessException;
 import ar.com.nextel.sfa.client.SolicitudRpcService;
 import ar.com.nextel.sfa.client.dto.SolicitudServicioDto;
 import ar.com.nextel.sfa.client.dto.SolicitudServicioRequestDto;
+import ar.com.nextel.sfa.server.businessservice.SolicitudBusinessService;
 import ar.com.nextel.sfa.server.util.MapperExtended;
 import ar.com.nextel.util.AppLogger;
 import ar.com.snoop.gwt.commons.client.exception.RpcExceptionMessages;
+import ar.com.snoop.gwt.commons.server.RemoteService;
 import ar.com.snoop.gwt.commons.server.util.ExceptionUtil;
 
-import com.google.gwt.user.server.rpc.RemoteServiceServlet;
-
-public class SolicitudRpcServiceImpl extends RemoteServiceServlet implements SolicitudRpcService {
+public class SolicitudRpcServiceImpl extends RemoteService implements SolicitudRpcService {
 
 	private MapperExtended mapper;
 	private WebApplicationContext context;
 	private SolicitudServicioBusinessOperator solicitudesBusinessOperator;
-	private final String CUENTA_FILTRADA = "Acceso denegado. No puede operar con esta cuenta.";
-	private FinancialSystem financialSystem;
+
+	private SolicitudBusinessService solicitudBusinessService;
+	private Repository repository;
 
 	public void init() throws ServletException {
 		super.init();
 		context = WebApplicationContextUtils.getWebApplicationContext(getServletContext());
 		mapper = (MapperExtended) context.getBean("dozerMapper");
 		solicitudesBusinessOperator = (SolicitudServicioBusinessOperator) context
-				.getBean("solicitudesBusinessOperator");
-		// financialSystem
+				.getBean("solicitudServicioBusinessOperatorBean");
+		solicitudBusinessService = (SolicitudBusinessService) context.getBean("solicitudBusinessService");
 	}
 
-	public SolicitudServicioDto createSolicitudServicio (
+	public SolicitudServicioDto createSolicitudServicio(
 			SolicitudServicioRequestDto solicitudServicioRequestDto) throws RpcExceptionMessages {
-		SolicitudServicioRequest request = (SolicitudServicioRequest) mapper
-				.mapFromDto(solicitudServicioRequestDto);
+		SolicitudServicioRequest request = mapper.map(solicitudServicioRequestDto,
+				SolicitudServicioRequest.class);
 		AppLogger.info("Creando Solicitud de Servicio con Request -> " + request.toString());
-
-		SolicitudServicioProviderResult providerResult = null;
+		SolicitudServicio solicitud = null;
 		try {
-			providerResult = this.solicitudesBusinessOperator.provideSolicitudServicio(request);
-		} catch (BusinessException e) {
+			solicitud = solicitudBusinessService.createSolicitudServicio(request);
+		} catch (Exception e) {
 			throw ExceptionUtil.wrap(e);
 		}
-
-		addCreditoFidelizacion(providerResult.getSolicitudServicio());
-
-		AccessAuthorization accessAuthorization = this.solicitudesBusinessOperator
-				.calculateAccessAuthorization(providerResult.getSolicitudServicio());
-
-		if (!accessAuthorization.hasSamePermissionsAs(AccessAuthorization.fullAccess())) {
-			accessAuthorization.setReasonPrefix(CUENTA_FILTRADA);
-			throw new RpcExceptionMessages(accessAuthorization.getReason());
-		}
-
-		SolicitudServicioDto solicitudServicioDto = mapper.map(providerResult.getSolicitudServicio(),
-				SolicitudServicioDto.class);
+		SolicitudServicioDto solicitudServicioDto = mapper.map(solicitud, SolicitudServicioDto.class);
 
 		AppLogger.info("Creacion de Solicitud de Servicio finalizada");
 		return solicitudServicioDto;
 	}
 
-	private void addCreditoFidelizacion(SolicitudServicio solicitudServicio) {
-		String numeroCuenta = solicitudServicio.getCuenta().getCodigoVantive();
-		try {
-			EncabezadoCreditoDTO creditoFidelizacion = getCreditoFidelizacion(numeroCuenta);
-			EstadoCreditoFidelizacion estadoCreditoFidelizacion = new EstadoCreditoFidelizacion(
-					solicitudServicio.getCuenta(), creditoFidelizacion.getMonto().doubleValue(),
-					creditoFidelizacion.getFechaVencimiento());
-
-			solicitudServicio.getCuenta().setEstadoCreditoFidelizacion(estadoCreditoFidelizacion);
-		} catch (FinancialSystemException e) {
-			e.printStackTrace();
-		}
-	}
-
-	private EncabezadoCreditoDTO getCreditoFidelizacion(String numeroCuenta) throws FinancialSystemException {
-		EncabezadoCreditoDTO retrieveEncabezadoCreditoFidelizacion = financialSystem
-				.retrieveEncabezadoCreditoFidelizacion(numeroCuenta);
-		return retrieveEncabezadoCreditoFidelizacion;
-	}
 }
---
+++ src/main/java/ar/com/nextel/sfa/server/businessservice/SolicitudBusinessService.java
@@ -0,0 +1,117 @@
+package ar.com.nextel.sfa.server.businessservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import ar.com.nextel.business.legacy.financial.FinancialSystem;
+import ar.com.nextel.business.legacy.financial.dto.EncabezadoCreditoDTO;
+import ar.com.nextel.business.legacy.financial.exception.FinancialSystemException;
+import ar.com.nextel.business.solicitudes.creation.SolicitudServicioBusinessOperator;
+import ar.com.nextel.business.solicitudes.creation.request.SolicitudServicioRequest;
+import ar.com.nextel.business.solicitudes.provider.SolicitudServicioProviderResult;
+import ar.com.nextel.components.accessMode.AccessAuthorization;
+import ar.com.nextel.framework.repository.Repository;
+import ar.com.nextel.model.cuentas.beans.EstadoCreditoFidelizacion;
+import ar.com.nextel.model.cuentas.beans.Vendedor;
+import ar.com.nextel.model.solicitudes.beans.SolicitudServicio;
+import ar.com.nextel.services.components.sessionContext.SessionContextLoader;
+import ar.com.nextel.services.exceptions.BusinessException;
+
+@Service
+public class SolicitudBusinessService {
+
+	private SolicitudServicioBusinessOperator solicitudesBusinessOperator;
+	private FinancialSystem financialSystem;
+	private SessionContextLoader sessionContextLoader;
+	private Repository repository;
+	private final String CUENTA_FILTRADA = "Acceso denegado. No puede operar con esta cuenta.";
+
+	@Autowired
+	public void setSolicitudesBusinessOperator(
+			SolicitudServicioBusinessOperator solicitudServicioBusinessOperatorBean) {
+		this.solicitudesBusinessOperator = solicitudServicioBusinessOperatorBean;
+	}
+
+	@Autowired
+	public void setFinancialSystem(FinancialSystem financialSystemBean) {
+		this.financialSystem = financialSystemBean;
+	}
+
+	@Autowired	
+	public void setSessionContextLoader(SessionContextLoader sessionContextLoader) {
+		this.sessionContextLoader = sessionContextLoader;
+	}
+	
+	@Autowired
+	public void setRepository(@Qualifier("repository")Repository repository) {
+		this.repository = repository;
+	}
+
+	@Transactional(readOnly = false, propagation = Propagation.REQUIRES_NEW)
+	public SolicitudServicio createSolicitudServicio(SolicitudServicioRequest solicitudServicioRequest) throws BusinessException {
+
+		SolicitudServicioProviderResult providerResult = null;
+			providerResult = this.solicitudesBusinessOperator
+					.provideSolicitudServicio(solicitudServicioRequest);
+
+		SolicitudServicio solicitud = providerResult.getSolicitudServicio();
+
+		addCreditoFidelizacion(solicitud);
+
+		AccessAuthorization accessAuthorization = this.solicitudesBusinessOperator
+				.calculateAccessAuthorization(providerResult.getSolicitudServicio());
+
+		if (!accessAuthorization.hasSamePermissionsAs(AccessAuthorization.fullAccess())) {
+			accessAuthorization.setReasonPrefix(CUENTA_FILTRADA);
+			throw new BusinessException(null, accessAuthorization.getReason());
+		}
+
+		// sessContext = (SessionContextLoader) context
+		// .getBean("sessionContextLoader");;
+		// RegistroVendedores registroVendedores;
+		// Usuario usuario = new Usuario();
+		// usuario.setUserName("acsa1");
+		// sessContext.getSessionContext().setVendedor(registroVendedores.getVendedor(usuario));
+
+		// Logica de negocio. Buscar lugar mas apto para ponerlo.
+		Vendedor vendedor = sessionContextLoader.getVendedor();
+
+		// if(no tiene permiso de edicion){
+		// solicitudServicio.getCuenta().terminarOperacion();
+		// }
+
+		solicitud.consultarCuentaPotencial();
+		if (providerResult.wasCreationNeeded()) {
+			if (!solicitud.getCuenta().isLockedForAnyone() || solicitud.getCuenta().isLockedFor(vendedor)) {
+				solicitud.getCuenta().iniciarOperacion(vendedor);
+			}
+			repository.save(solicitud);
+		}
+
+		return solicitud;
+	}
+
+	private void addCreditoFidelizacion(SolicitudServicio solicitudServicio) {
+		String numeroCuenta = solicitudServicio.getCuenta().getCodigoVantive();
+		try {
+			EncabezadoCreditoDTO creditoFidelizacion = getCreditoFidelizacion(numeroCuenta);
+			EstadoCreditoFidelizacion estadoCreditoFidelizacion = new EstadoCreditoFidelizacion(
+					solicitudServicio.getCuenta(), creditoFidelizacion.getMonto().doubleValue(),
+					creditoFidelizacion.getFechaVencimiento());
+
+			solicitudServicio.getCuenta().setEstadoCreditoFidelizacion(estadoCreditoFidelizacion);
+		} catch (FinancialSystemException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private EncabezadoCreditoDTO getCreditoFidelizacion(String numeroCuenta) throws FinancialSystemException {
+		EncabezadoCreditoDTO retrieveEncabezadoCreditoFidelizacion = financialSystem
+				.retrieveEncabezadoCreditoFidelizacion(numeroCuenta);
+		return retrieveEncabezadoCreditoFidelizacion;
+	}
+
+}
