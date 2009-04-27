cb599b6de49a20b47c481ba52b87cc343ac14fa8
jlgperez <jlgperez>
2009-04-27 18:22:59 +0000

    sin terminar.

--- src/main/java/ar/com/nextel/sfa/client/ss/DatosSSUI.java
+++ src/main/java/ar/com/nextel/sfa/client/ss/DatosSSUI.java
@@ -8,14 +8,16 @@ import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class DatosSSUI extends Composite {
 
 	private FlowPanel mainpanel;
-	private CrearSSUIData crearSSUIData;
+	private EditarSSUIData crearSSUIData;
+	private Grid detalleSS;
 
-	public DatosSSUI(CrearSSUIData crearSSUIData) {
+	public DatosSSUI(EditarSSUIData crearSSUIData) {
 		mainpanel = new FlowPanel();
 		initWidget(mainpanel);
 		this.crearSSUIData = crearSSUIData;
@@ -29,18 +31,23 @@ public class DatosSSUI extends Composite {
 		layout.addStyleName("layout");
 		layout.setHTML(0, 0, Sfa.constant().nss());
 		layout.setWidget(0, 1, crearSSUIData.getNss());
-		layout.setHTML(0, 2, Sfa.constant().nss());
-		layout.setWidget(0, 3, crearSSUIData.getNss());
-		layout.setHTML(0, 4, Sfa.constant().nss());
-		layout.setWidget(0, 5, crearSSUIData.getNss());
+		layout.setHTML(0, 2, Sfa.constant().nflota());
+		layout.setWidget(0, 3, crearSSUIData.getNflota());
+		layout.setHTML(0, 4, Sfa.constant().origen());
+		layout.setWidget(0, 5, crearSSUIData.getOrigen());
 		return layout;
 	}
 
 	private Widget getDomicilioPanel() {
 		TitledPanel domicilio = new TitledPanel("Domicilio");
-		Button crearDomicilio = new Button("crear nuevo");
-		domicilio.add(crearDomicilio);
+		Button crearDomicilio = new Button("Crear nuevo");
+		crearDomicilio.addStyleName("crearDomicilioButton");
+		SimplePanel crearDomicilioWrapper = new SimplePanel();
+		crearDomicilioWrapper.add(crearDomicilio);
+		crearDomicilioWrapper.addStyleName("crearDomicilioBWrapper");
+		domicilio.add(crearDomicilioWrapper);
 		Grid layoutDomicilio = new Grid(3, 4);
+		layoutDomicilio.addStyleName("layout");
 		layoutDomicilio.setHTML(0, 0, Sfa.constant().entrega());
 		layoutDomicilio.setWidget(0, 1, crearSSUIData.getEntrega());
 		layoutDomicilio.setWidget(0, 2, IconFactory.lapiz());
@@ -51,18 +58,28 @@ public class DatosSSUI extends Composite {
 		layoutDomicilio.setWidget(1, 3, IconFactory.cancel());
 		layoutDomicilio.setHTML(2, 0, Sfa.constant().aclaracion());
 		layoutDomicilio.setWidget(2, 1, crearSSUIData.getAclaracion());
+		layoutDomicilio.getColumnFormatter().setWidth(1, "500px");
 		domicilio.add(layoutDomicilio);
 		return domicilio;
 	}
 
 	private Widget getDetallePanel() {
 		TitledPanel detalle = new TitledPanel("Detalle");
-		Grid detalleSS = new Grid(1, 11);
+		SimplePanel wrapper = new SimplePanel();
+		wrapper.addStyleName("resumenSSTableWrapper mlr5");
+		detalleSS = new Grid(1, 11);
 		String[] titles = { "Item", "Pcio Vta.", "Alias", "Plan", "Pcio Vta. Plan", "Localidad",
 				"Nº Reserva", "Tipo SS", "Cant." };
 		for (int i = 0; i < titles.length; i++) {
 			detalleSS.setHTML(0, i + 2, titles[i]);
 		}
+		detalleSS.setCellPadding(0);
+		detalleSS.setCellSpacing(0);
+		detalleSS.addStyleName("dataTable");
+		detalleSS.setWidth("98%");
+		detalleSS.getRowFormatter().addStyleName(0, "header");
+		wrapper.add(detalleSS);
+		detalle.add(wrapper);
 		return detalle;
 	}
 }
--- src/main/java/ar/com/nextel/sfa/client/ss/VariosSSUI.java
+++ src/main/java/ar/com/nextel/sfa/client/ss/VariosSSUI.java
@@ -5,42 +5,125 @@ import ar.com.nextel.sfa.client.widget.TitledPanel;
 
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.InlineLabel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
 
-public class VariosSSUI extends Composite{
+public class VariosSSUI extends Composite {
 
 	private FlowPanel mainpanel;
-	private CrearSSUIData crearSSUIData;
-	
-	public VariosSSUI(CrearSSUIData crearSSUIData) {
+	private EditarSSUIData editarSSUIData;
+
+	public VariosSSUI(EditarSSUIData editarSSUIData) {
 		mainpanel = new FlowPanel();
 		initWidget(mainpanel);
-		this.crearSSUIData = crearSSUIData;
-		
+		this.editarSSUIData = editarSSUIData;
+
+		mainpanel.add(getCreditoFidelizacion());
+		mainpanel.add(getPataconex());
+		mainpanel.add(getFirmas());
+		mainpanel.add(getAnticipo());
+		mainpanel.add(getResumen());
+		mainpanel.add(getScoring());
+
 	}
-	
-	private Widget getPataconex(){
+
+	private Widget getPataconex() {
 		TitledPanel pataconexPanel = new TitledPanel(Sfa.constant().pataconexTitle());
+		InlineLabel pataconexLabel = new InlineLabel(Sfa.constant().pataconex());
+		pataconexLabel.addStyleName("mlr5");
+		pataconexPanel.add(pataconexLabel);
+		pataconexPanel.add(editarSSUIData.getPataconex());
 		return pataconexPanel;
 	}
-	
-	private Widget getCreditoFidelizacion(){
+
+	private Widget getCreditoFidelizacion() {
 		TitledPanel creditoPanel = new TitledPanel(Sfa.constant().creditoFidelizacionTitle());
-		return creditoPanel;	
+		Grid layout = new Grid(1, 6);
+		layout.addStyleName("layout");
+		layout.setHTML(0, 0, Sfa.constant().disponible());
+		layout.setWidget(0, 1, editarSSUIData.getCredFidelDisponibleText());
+		layout.setHTML(0, 2, Sfa.constant().vencimiento());
+		layout.setWidget(0, 3, editarSSUIData.getCredFidelVencimientoText());
+		layout.setHTML(0, 4, Sfa.constant().autilizar());
+		layout.setWidget(0, 5, editarSSUIData.getCredFidelizacion());
+		layout.getColumnFormatter().setWidth(1, "120px");
+		layout.getColumnFormatter().setWidth(3, "120px");
+		creditoPanel.add(layout);
+		return creditoPanel;
 	}
-	
-	private Widget getFirmas(){
+
+	private Widget getFirmas() {
 		TitledPanel firmasPanel = new TitledPanel(Sfa.constant().firmaTitle());
-		return firmasPanel;		
+		InlineLabel pataconexLabel = new InlineLabel(Sfa.constant().firmarSS());
+		pataconexLabel.addStyleName("mlr5");
+		firmasPanel.add(pataconexLabel);
+		firmasPanel.add(editarSSUIData.getFirmarss());
+		return firmasPanel;
 	}
-	
-	private Widget getResumen(){
-		TitledPanel resumenPanel = new TitledPanel(Sfa.constant().firmaTitle());
-		return resumenPanel;	
+
+	private Widget getAnticipo() {
+		FlowPanel anticipoPanel = new FlowPanel();
+		Grid anticipoTable = new Grid(2, 2);
+		anticipoTable.addStyleName("layout mlr5");
+		anticipoTable.setHTML(0, 0, Sfa.constant().anticipo());
+		anticipoTable.setWidget(0, 1, editarSSUIData.getAnticipo());
+		anticipoTable.setHTML(1, 0, Sfa.constant().observaciones());
+		anticipoTable.setWidget(1, 1, editarSSUIData.getObservaciones());
+		anticipoPanel.add(anticipoTable);
+		return anticipoPanel;
 	}
-	
-	private Widget getScoring(){
+
+	private Widget getResumen() {
+		TitledPanel resumenPanel = new TitledPanel(Sfa.constant().resumentSSTitle());
+		resumenPanel.add(getResumenTable());
+		Label totalesLabel = new Label(Sfa.constant().totales());
+		resumenPanel.add(totalesLabel);
+		Grid totales = new Grid(2, 6);
+		totales.addStyleName("layout");
+		totales.setHTML(0, 0, Sfa.constant().precioLista());
+		totales.setWidget(0, 1, editarSSUIData.getPrecioListaText());
+		totales.setHTML(0, 2, Sfa.constant().desvios());
+		totales.setWidget(0, 3, editarSSUIData.getDesvioText());
+		totales.setHTML(0, 4, Sfa.constant().creditoFidel());
+		totales.setWidget(0, 5, editarSSUIData.getCredFidelText());
+		totales.setHTML(1, 0, Sfa.constant().pataconex());
+		totales.setWidget(1, 1, editarSSUIData.getPataconexText());
+		totales.setHTML(1, 2, Sfa.constant().precioVenta());
+		totales.setWidget(1, 3, editarSSUIData.getPrecioVentaText());
+
+		totales.getColumnFormatter().setWidth(1, "120px");
+		totales.getColumnFormatter().setWidth(3, "120px");
+
+		resumenPanel.add(totales);
+		return resumenPanel;
+	}
+
+	private Widget getResumenTable() {
+		SimplePanel wrapper = new SimplePanel();
+		wrapper.addStyleName("detalleSSTableWrapper mlr5");
+		Grid resumenSS = new Grid(1, 8);
+		String[] titles = { "Item", "Cant.", "Forma Contrat.", "Precio Item", "Plan", "Precio Plan",
+				"Precio Gtía.", "Serv. Adic." };
+		for (int i = 0; i < titles.length; i++) {
+			resumenSS.setHTML(0, i, titles[i]);
+		}
+		resumenSS.setCellPadding(0);
+		resumenSS.setCellSpacing(0);
+		resumenSS.addStyleName("dataTable");
+		resumenSS.setWidth("98%");
+		resumenSS.getRowFormatter().addStyleName(0, "header");
+		wrapper.add(resumenSS);
+		return wrapper;
+	}
+
+	private Widget getScoring() {
 		TitledPanel scoringPanel = new TitledPanel(Sfa.constant().firmaTitle());
-		return scoringPanel;	
+		HTML consultarVeraz = new HTML("Consultar Veraz");
+		scoringPanel.add(consultarVeraz);
+		return scoringPanel;
 	}
 }
