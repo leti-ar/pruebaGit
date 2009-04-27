d2844bdf1d9ecf343affa591ade5827b09d730c4
jlgperez <jlgperez>
2009-04-27 18:27:07 +0000

    +getSolicitudInitializer

--- src/main/java/ar/com/nextel/sfa/server/SolicitudRpcServiceImpl.java
+++ src/main/java/ar/com/nextel/sfa/server/SolicitudRpcServiceImpl.java
@@ -2,29 +2,20 @@ package ar.com.nextel.sfa.server;
 
 import javax.servlet.ServletException;
 
-import org.apache.commons.collections.Closure;
-import org.quartz.impl.jdbcjobstore.CloudscapeDelegate;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
-import ar.com.nextel.business.legacy.financial.FinancialSystem;
-import ar.com.nextel.business.legacy.financial.dto.EncabezadoCreditoDTO;
-import ar.com.nextel.business.legacy.financial.exception.FinancialSystemException;
-import ar.com.nextel.business.solicitudes.creation.SolicitudServicioBusinessOperator;
 import ar.com.nextel.business.solicitudes.creation.request.SolicitudServicioRequest;
-import ar.com.nextel.business.solicitudes.provider.SolicitudServicioProviderResult;
-import ar.com.nextel.business.vendedores.RegistroVendedores;
-import ar.com.nextel.components.accessMode.AccessAuthorization;
 import ar.com.nextel.framework.repository.Repository;
-import ar.com.nextel.framework.security.Usuario;
-import ar.com.nextel.model.cuentas.beans.EstadoCreditoFidelizacion;
-import ar.com.nextel.model.cuentas.beans.Vendedor;
+import ar.com.nextel.model.solicitudes.beans.OrigenSolicitud;
 import ar.com.nextel.model.solicitudes.beans.SolicitudServicio;
-import ar.com.nextel.services.components.sessionContext.SessionContextLoader;
-import ar.com.nextel.services.exceptions.BusinessException;
+import ar.com.nextel.model.solicitudes.beans.TipoAnticipo;
 import ar.com.nextel.sfa.client.SolicitudRpcService;
+import ar.com.nextel.sfa.client.dto.OrigenSolicitudDto;
 import ar.com.nextel.sfa.client.dto.SolicitudServicioDto;
 import ar.com.nextel.sfa.client.dto.SolicitudServicioRequestDto;
+import ar.com.nextel.sfa.client.dto.TipoAnticipoDto;
+import ar.com.nextel.sfa.client.initializer.SolicitudInitializer;
 import ar.com.nextel.sfa.server.businessservice.SolicitudBusinessService;
 import ar.com.nextel.sfa.server.util.MapperExtended;
 import ar.com.nextel.util.AppLogger;
@@ -36,7 +27,7 @@ public class SolicitudRpcServiceImpl extends RemoteService implements SolicitudR
 
 	private MapperExtended mapper;
 	private WebApplicationContext context;
-	private SolicitudServicioBusinessOperator solicitudesBusinessOperator;
+	// private SolicitudServicioBusinessOperator solicitudesBusinessOperator;
 
 	private SolicitudBusinessService solicitudBusinessService;
 	private Repository repository;
@@ -45,9 +36,10 @@ public class SolicitudRpcServiceImpl extends RemoteService implements SolicitudR
 		super.init();
 		context = WebApplicationContextUtils.getWebApplicationContext(getServletContext());
 		mapper = (MapperExtended) context.getBean("dozerMapper");
-		solicitudesBusinessOperator = (SolicitudServicioBusinessOperator) context
-				.getBean("solicitudServicioBusinessOperatorBean");
+		// solicitudesBusinessOperator = (SolicitudServicioBusinessOperator) context
+		// .getBean("solicitudServicioBusinessOperatorBean");
 		solicitudBusinessService = (SolicitudBusinessService) context.getBean("solicitudBusinessService");
+		repository = (Repository) context.getBean("repository");
 	}
 
 	public SolicitudServicioDto createSolicitudServicio(
@@ -67,4 +59,16 @@ public class SolicitudRpcServiceImpl extends RemoteService implements SolicitudR
 		return solicitudServicioDto;
 	}
 
+	public SolicitudInitializer getSolicitudInitializer() {
+		SolicitudInitializer initializer = new SolicitudInitializer();
+		initializer.setOrigenesSolicitud(mapper.convertList(repository.getAll(OrigenSolicitud.class),
+				OrigenSolicitudDto.class));
+		initializer.setTiposAnticipo(mapper.convertList(repository.getAll(TipoAnticipo.class),
+				TipoAnticipoDto.class));
+		return initializer;
+	}
+
+	public SolicitudServicioDto saveSolicituServicio(SolicitudServicioDto solicitudServicioDto) {
+		return solicitudServicioDto;
+	}
 }
