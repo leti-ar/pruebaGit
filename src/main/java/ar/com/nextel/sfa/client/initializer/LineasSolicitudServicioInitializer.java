aa291732ae2d9b6a55ec8b966ca7f25a9b42c4aa
jlgperez <jlgperez>
2009-05-18 20:21:59 +0000

    + tiposPlanes; + localidades; - listaDePrecios

--- src/main/java/ar/com/nextel/sfa/client/initializer/LineasSolicitudServicioInitializer.java
+++ src/main/java/ar/com/nextel/sfa/client/initializer/LineasSolicitudServicioInitializer.java
@@ -2,7 +2,8 @@ package ar.com.nextel.sfa.client.initializer;
 
 import java.util.List;
 
-import ar.com.nextel.sfa.client.dto.ListaPreciosDto;
+import ar.com.nextel.sfa.client.dto.LocalidadDto;
+import ar.com.nextel.sfa.client.dto.TipoPlanDto;
 import ar.com.nextel.sfa.client.dto.TipoSolicitudDto;
 
 import com.google.gwt.user.client.rpc.IsSerializable;
@@ -10,7 +11,8 @@ import com.google.gwt.user.client.rpc.IsSerializable;
 public class LineasSolicitudServicioInitializer implements IsSerializable {
 
 	private List<TipoSolicitudDto> tiposSolicitudes;
-	private List<ListaPreciosDto> listasPrecios;
+	private List<TipoPlanDto> tiposPlanes;
+	private List<LocalidadDto> localidades;
 
 	public void setTiposSolicitudes(List<TipoSolicitudDto> tiposSolicitudes) {
 		this.tiposSolicitudes = tiposSolicitudes;
@@ -20,12 +22,20 @@ public class LineasSolicitudServicioInitializer implements IsSerializable {
 		return tiposSolicitudes;
 	}
 
-	public List<ListaPreciosDto> getListasPrecios() {
-		return listasPrecios;
+	public List<TipoPlanDto> getTiposPlanes() {
+		return tiposPlanes;
 	}
 
-	public void setListasPrecios(List<ListaPreciosDto> listasPrecios) {
-		this.listasPrecios = listasPrecios;
+	public void setTiposPlanes(List<TipoPlanDto> tiposPlanes) {
+		this.tiposPlanes = tiposPlanes;
+	}
+
+	public List<LocalidadDto> getLocalidades() {
+		return localidades;
+	}
+
+	public void setLocalidades(List<LocalidadDto> localidades) {
+		this.localidades = localidades;
 	}
 
 }
