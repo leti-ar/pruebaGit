c8e8bc5046e9b5fe9af668ed7a38714627094ffe
jlgperez <jlgperez>
2009-05-18 20:20:49 +0000

    implementa ListBoxItem

--- src/main/java/ar/com/nextel/sfa/client/dto/TipoPlanDto.java
+++ src/main/java/ar/com/nextel/sfa/client/dto/TipoPlanDto.java
@@ -1,19 +1,43 @@
 package ar.com.nextel.sfa.client.dto;
 
+import ar.com.snoop.gwt.commons.client.dto.ListBoxItem;
+
 import com.google.gwt.user.client.rpc.IsSerializable;
 
-public class TipoPlanDto implements IsSerializable {
-    private String descripcion;
-    private String codigoBSCS;
+public class TipoPlanDto implements IsSerializable, ListBoxItem {
+
+	private Long id;
+	private String descripcion;
+	private String codigoBSCS;
+
+	public String getItemText() {
+		return descripcion;
+	}
+
+	public String getItemValue() {
+		return "" + id;
+	}
+
 	public String getDescripcion() {
 		return descripcion;
 	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
+
 	public String getCodigoBSCS() {
 		return codigoBSCS;
 	}
+
 	public void setCodigoBSCS(String codigoBSCS) {
 		this.codigoBSCS = codigoBSCS;
 	}
