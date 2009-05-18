20dc7c137a133c6f0ba9875cb5b97bed3935a682
jlgperez <jlgperez>
2009-05-18 20:17:01 +0000

    nuevo

---
+++ src/main/java/ar/com/nextel/sfa/client/dto/LocalidadDto.java
@@ -0,0 +1,36 @@
+package ar.com.nextel.sfa.client.dto;
+
+import ar.com.snoop.gwt.commons.client.dto.ListBoxItem;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+public class LocalidadDto implements IsSerializable, ListBoxItem {
+
+	private Long id;
+	private String descripcion;
+
+	public String getItemText() {
+		return descripcion;
+	}
+
+	public String getItemValue() {
+		return "" + id;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+}
