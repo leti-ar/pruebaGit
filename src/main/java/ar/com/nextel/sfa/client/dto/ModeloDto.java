c4dab8291fd9395746a0eeab2e3c21f501220191
jlgperez <jlgperez>
2009-05-19 19:56:28 +0000

    +

---
+++ src/main/java/ar/com/nextel/sfa/client/dto/ModeloDto.java
@@ -0,0 +1,17 @@
+package ar.com.nextel.sfa.client.dto;
+
+import ar.com.snoop.gwt.commons.client.dto.ListBoxItem;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+public class ModeloDto extends EnumDto implements ListBoxItem, IsSerializable {
+
+	public String getItemText() {
+		return descripcion;
+	}
+
+	public String getItemValue() {
+		return "" + id;
+	}
+
+}
