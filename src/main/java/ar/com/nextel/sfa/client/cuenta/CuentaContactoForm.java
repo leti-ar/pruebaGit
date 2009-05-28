4136f1247c3378ce95272883ae8ba2d3d9fde62d
farambar <farambar>
2009-05-28 20:00:32 +0000

    solapa contactos (cargar la tabla con los contactos de la cuenta)

--- src/main/java/ar/com/nextel/sfa/client/cuenta/CuentaContactoForm.java
+++ src/main/java/ar/com/nextel/sfa/client/cuenta/CuentaContactoForm.java
@@ -1,17 +1,30 @@
 package ar.com.nextel.sfa.client.cuenta;
 
-import ar.com.nextel.sfa.client.constant.Sfa;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
+import ar.com.nextel.sfa.client.constant.Sfa;
+import ar.com.nextel.sfa.client.dto.ContactoDto;
+import ar.com.nextel.sfa.client.dto.TelefonoDto;
+import ar.com.nextel.sfa.client.image.IconFactory;
+import ar.com.nextel.sfa.client.widget.MessageDialog;
+
+import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.SourcesTableEvents;
+import com.google.gwt.user.client.ui.TableListener;
 import com.google.gwt.user.client.ui.Widget;
 
 public class CuentaContactoForm extends Composite {
 
 	private FlexTable mainPanel;
-
+	FlexTable datosTabla = new FlexTable();
+	private List<ContactoDto> listaContactos = new ArrayList();
+		
 	private static CuentaContactoForm instance = null;
 
 	public static CuentaContactoForm getInstance() {
@@ -25,13 +38,13 @@ public class CuentaContactoForm extends Composite {
 
 		mainPanel = new FlexTable();
 		mainPanel.setWidth("100%");
-		FlexTable datosTabla = new FlexTable();
 		initWidget(mainPanel);
 		mainPanel.addStyleName("gwt-BuscarCuentaResultTable");
 		mainPanel.setCellPadding(20);
 		Button crear = new Button("Crear nuevo");
 		crear.addStyleName("crearDomicilioButton");
 		mainPanel.setWidget(0, 0, crear);
+		datosTabla.addTableListener(new Listener());
 		initTable(datosTabla);
 		mainPanel.getColumnFormatter().addStyleName(0, "alignRight");
 		mainPanel.getRowFormatter().addStyleName(0, "alignRight");
@@ -49,7 +62,7 @@ public class CuentaContactoForm extends Composite {
 
 	private void initTable(FlexTable table) {
 
-		String[] widths = { "24px", "24px", "150px", "150px", "150px", "65%", };
+		String[] widths = { "24px", "24px", "150px", "150px", "200px", "45%", };
 		for (int col = 0; col < widths.length; col++) {
 			table.getColumnFormatter().setWidth(col, widths[col]);
 		}
@@ -66,6 +79,70 @@ public class CuentaContactoForm extends Composite {
 		table.setHTML(0, 3, Sfa.constant().apellido());
 		table.setHTML(0, 4, Sfa.constant().telefono());
 		table.setHTML(0, 5, Sfa.constant().whiteSpace());
+	}
+	
+	
+	public void cargarTabla() {
+		int row = 1;
+		if (listaContactos != null) {
+			for (Iterator iter = listaContactos.iterator(); iter.hasNext();) {
+				ContactoDto contacto = (ContactoDto) iter.next();
+				datosTabla.setWidget(row, 0, IconFactory.lapiz());
+				datosTabla.setWidget(row, 1, IconFactory.cancel());
+				datosTabla.setHTML(row, 2, contacto.getPersonaDto().getNombre());
+				datosTabla.setHTML(row, 3, contacto.getPersonaDto().getApellido());
+				datosTabla.setHTML(row, 4, obtenerTelefonoPrincipal(contacto));
+				datosTabla.setHTML(row, 5, Sfa.constant().whiteSpace());
+				row++;
+			} 
+		} 
+		while(datosTabla.getRowCount() > row){
+			datosTabla.removeRow(row++);
+		}
+	}		
+	
+	
+	
+	public void setearContactos(ContactoDto contactoDto) {
+		listaContactos.add(contactoDto);
+		cargarTabla();
+	}
 
+	
+	private class Listener implements TableListener {
+		public void onCellClicked(SourcesTableEvents arg0, int fila, int columna) {
+			if ((fila>=1) && (columna==1)) {
+				//ContactoDto contactoSelected = listaContactos.get(fila-1);
+				MessageDialog.getInstance().setDialogTitle("Eliminar Contacto");
+				MessageDialog.getInstance().setSize("300px", "100px");
+				MessageDialog.getInstance().showAceptarCancelar("¿Esta seguro que desea eliminar el contacto seleccionado?",getComandoAceptar(fila-1),MessageDialog.getInstance().getCloseCommand());
+			}
+		}
+	}
+	
+	private Command getComandoAceptar(final int numeroContacto){
+		Command comandoAceptar = new Command() {
+			public void execute() {
+				listaContactos.remove(numeroContacto);
+				MessageDialog.getInstance().hide();
+				cargarTabla();
+			}
+		};
+	return comandoAceptar;
+	}	
+	
+	public String obtenerTelefonoPrincipal(ContactoDto contactoDto) {
+		List<TelefonoDto> listaTelefonos = new ArrayList();
+		listaTelefonos = contactoDto.getPersonaDto().getTelefonos();
+		
+		if (listaTelefonos != null) {
+			for (Iterator iter = listaTelefonos.iterator(); iter.hasNext();) {
+				TelefonoDto telefonoDto = (TelefonoDto) iter.next();
+				if (telefonoDto.getPrincipal()) {
+					return ("(" + telefonoDto.getArea() + ") " + telefonoDto.getNumeroLocal() + "(" + telefonoDto.getInterno() + ")");
+				}
+			}
+		}
+		return null;
 	}
 }
