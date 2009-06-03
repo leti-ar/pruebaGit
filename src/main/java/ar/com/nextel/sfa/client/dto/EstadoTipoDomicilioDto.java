279959d89376b16520aa90e792c1f37b2f783306
jlgperez <jlgperez>
2009-06-03 18:12:34 +0000

    +getListBoxItems

--- src/main/java/ar/com/nextel/sfa/client/dto/EstadoTipoDomicilioDto.java
+++ src/main/java/ar/com/nextel/sfa/client/dto/EstadoTipoDomicilioDto.java
@@ -1,11 +1,17 @@
 package ar.com.nextel.sfa.client.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.com.snoop.gwt.commons.client.dto.ListBoxItem;
+import ar.com.snoop.gwt.commons.client.dto.ListBoxItemImpl;
+
 public enum EstadoTipoDomicilioDto {
 	PRINCIPAL(2L, "Principal"), SI(0L, "Si"), NO(1L, "No");
-	
+
 	private Long id;
 	private String descripcion;
-		
+
 	private EstadoTipoDomicilioDto(Long id, String descripcion) {
 		this.id = id;
 		this.descripcion = descripcion;
@@ -17,5 +23,13 @@ public enum EstadoTipoDomicilioDto {
 
 	public String getDescripcion() {
 		return descripcion;
+	}
+
+	public static List<ListBoxItem> getListBoxItems() {
+		List<ListBoxItem> lista = new ArrayList();
+		for (int i = 0; i < values().length; i++) {
+			lista.add(new ListBoxItemImpl(values()[i].getDescripcion(), "" + values()[i].getId()));
+		}
+		return lista;
 	}
 }
