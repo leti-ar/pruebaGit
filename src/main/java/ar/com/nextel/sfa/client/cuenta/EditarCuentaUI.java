4617d7261729c0183956a5e353669113a9f3b393
ebechtol <ebechtol>
2009-06-10 21:43:19 +0000

    seteo de tab datos como el de inicio

--- src/main/java/ar/com/nextel/sfa/client/cuenta/EditarCuentaUI.java
+++ src/main/java/ar/com/nextel/sfa/client/cuenta/EditarCuentaUI.java
@@ -1,25 +1,21 @@
 package ar.com.nextel.sfa.client.cuenta;
 
 import ar.com.nextel.sfa.client.CuentaRpcService;
-import ar.com.nextel.sfa.client.context.ClientContext;
-import ar.com.nextel.sfa.client.dto.CuentaDto;
 import ar.com.nextel.sfa.client.dto.DocumentoDto;
+import ar.com.nextel.sfa.client.dto.GranCuentaDto;
 import ar.com.nextel.sfa.client.dto.TipoDocumentoDto;
 import ar.com.nextel.sfa.client.util.HistoryUtils;
 import ar.com.nextel.sfa.client.widget.ApplicationUI;
+import ar.com.nextel.sfa.client.widget.UILoader;
 import ar.com.snoop.gwt.commons.client.service.DefaultWaitCallback;
 import ar.com.snoop.gwt.commons.client.widget.dialog.ErrorDialog;
-
-import com.google.gwt.user.client.History;
 
 /**
  * @author eSalvador
  **/
 public class EditarCuentaUI extends ApplicationUI {
 
-
 	private final CuentaEdicionTabPanel cuentaTab = CuentaEdicionTabPanel.getInstance();
-//	private UserCenterDto userCenter;
 	
 	public EditarCuentaUI() {
 		super();
@@ -27,13 +23,14 @@ public class EditarCuentaUI extends ApplicationUI {
 
 	public boolean load() {
 		cuentaTab.clean();
+		cuentaTab.getTabPanel().selectTab(0);
 		
 		//viene de popup "Agregar"
-		if (HistoryUtils.getParam("nroDoc")!=null) {
+		if (HistoryUtils.getParam("nroDoc")!=null) {		
 			TipoDocumentoDto tipoDoc = new TipoDocumentoDto(Long.parseLong(HistoryUtils.getParam("tipoDoc")),null);
 			DocumentoDto docDto = new DocumentoDto(HistoryUtils.getParam("nroDoc"),tipoDoc);
-			CuentaRpcService.Util.getInstance().reservaCreacionCuenta(docDto,new DefaultWaitCallback<CuentaDto>() {
-				public void success(CuentaDto cuentaDto) {
+			CuentaRpcService.Util.getInstance().reservaCreacionCuenta(docDto,new DefaultWaitCallback<GranCuentaDto>() {
+				public void success(GranCuentaDto cuentaDto) {
 					cuentaTab.setCuenta2editDto(cuentaDto);
 					cuentaTab.validarCompletitudButton.addStyleName(cuentaTab.VALIDAR_COMPLETITUD_FAIL_STYLE);
 					cuentaTab.getCuentaDatosForm().setAtributosCamposAlAgregarCuenta(cuentaDto);
@@ -44,19 +41,35 @@ public class EditarCuentaUI extends ApplicationUI {
 		} else {//viene de resultado de busqueda
 			Long cuentaID = Long.parseLong(HistoryUtils.getParam("cuenta_id"));
 			String cod_vantive = HistoryUtils.getParam("cod_vantive");
-				// Si algunos de los dos tiene datos validos, carga los paneles.
-				CuentaRpcService.Util.getInstance().selectCuenta(cuentaID, cod_vantive,new DefaultWaitCallback<CuentaDto>() {
-					public void success(CuentaDto ctaDto) {
-						if (!ClientContext.getInstance().getUsuario().getId().equals(ctaDto.getVendedor().getId())) {
-							History.back();
-							ErrorDialog.getInstance().show("No tiene permiso para ver esa cuenta.");
-						}else{
-							cuentaTab.setCuenta2editDto(ctaDto);
-							cuentaTab.getCuentaDatosForm().setAtributosCamposAlMostrarResuladoBusqueda(ctaDto);
-							cargaPaneles();
+
+			//TODO: Sacar y hacer como corresponda. Solo se creo para emular la funcionalidad visualmente!
+			//Si la cuenta o el codigoVantive NO son nulos:
+			if ((cuentaID!=null) || (!cod_vantive.equals("null"))) {
+				//Si el codVantive es '***' o la cuenta es igual a 0:
+				// lanza mensaje de error por permisos y pone invisible los paneles.
+				if("***".equals(cod_vantive) || (cuentaID == 0)){
+					//panelVisible = false;
+					mainPanel.clear();
+					new BuscarCuentaUI().load();
+					UILoader.getInstance().setPage(0);
+					ErrorDialog.getInstance().show("No tiene permiso para ver esa cuenta.");
+				}else{
+					// Si algunos de los dos tiene datos validos, carga los paneles.
+					CuentaRpcService.Util.getInstance().selectCuenta(cuentaID, cod_vantive,new DefaultWaitCallback<GranCuentaDto>() {
+						public void success(GranCuentaDto ctaDto) {
+							//De todas formas, el servicio puede NO devolver ninguna cuenta: (Se pone el panel invisible!).
+							if (ctaDto == null){
+								mainPanel.clear();
+								new BuscarCuentaUI().load();
+							} else {
+								cuentaTab.setCuenta2editDto(ctaDto);
+								cuentaTab.getCuentaDatosForm().setAtributosCamposAlMostrarResuladoBusqueda(ctaDto);
+								cargaPaneles();
+							}
 						}
-					}
-				});
+					});
+				}
+			}
 		}
 		return true;
 	}
@@ -71,6 +84,8 @@ public class EditarCuentaUI extends ApplicationUI {
 		//Busca la cuenta con alguno de los dos datos NO nulos.
 		if(cuentaTab.getCuenta2editDto().getCodigoVantive() != null){
 			cuentaTab.setCliente(cuentaTab.getCuenta2editDto().getCodigoVantive());
+		}else{
+			cuentaTab.setCliente("***");
 		}
 		cuentaTab.setRazonSocial(cuentaTab.getCuenta2editDto().getPersona()!=null ? cuentaTab.getCuenta2editDto().getPersona().getRazonSocial():"");
 		//carga info pestaña Datos
@@ -79,14 +94,11 @@ public class EditarCuentaUI extends ApplicationUI {
 		if (cuentaTab.getCuenta2editDto().getPersona() != null) {
 			cuentaTab.getCuentaDomicilioForm().cargaTablaDomicilios(cuentaTab.getCuenta2editDto());
 		}
-		//TODO: Revisar esto: La CuentaDto no tiene CONTACTOS!!!! Antes no pinchaba porque estaba mal, se referenciaba a una GRAN CUENTA!!! ARREGLAR!!!
-
 		//carga info pestaña Contactos
-		//if (cuentaTab.getCuenta2editDto().getContactos() != null) {
-		//	cuentaTab.getCuentaContactoForm().setListaContactos(cuentaTab.getCuenta2editDto().getContactos());
+		if (cuentaTab.getCuenta2editDto().getContactos() != null) {
+			cuentaTab.getCuentaContactoForm().setListaContactos(cuentaTab.getCuenta2editDto().getContactos());
 			cuentaTab.getCuentaContactoForm().cargarTabla();
-		//}
-			
+		}
 		mainPanel.add(cuentaTab.getCuentaEdicionPanel());
 	}
 
