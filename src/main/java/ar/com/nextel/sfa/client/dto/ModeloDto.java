1891d0814120263200425415d1eebc28dfce8b8e
jlgperez <jlgperez>
2009-06-18 21:56:59 +0000

    agrego los items de cada modelo

--- src/main/java/ar/com/nextel/sfa/client/dto/ModeloDto.java
+++ src/main/java/ar/com/nextel/sfa/client/dto/ModeloDto.java
@@ -1,10 +1,14 @@
 package ar.com.nextel.sfa.client.dto;
 
+import java.util.List;
+
 import ar.com.snoop.gwt.commons.client.dto.ListBoxItem;
 
 import com.google.gwt.user.client.rpc.IsSerializable;
 
 public class ModeloDto extends EnumDto implements ListBoxItem, IsSerializable {
+
+	private List<ItemSolicitudTasadoDto> items;
 
 	public String getItemText() {
 		return descripcion;
@@ -12,6 +16,14 @@ public class ModeloDto extends EnumDto implements ListBoxItem, IsSerializable {
 
 	public String getItemValue() {
 		return "" + id;
+	}
+
+	public List<ItemSolicitudTasadoDto> getItems() {
+		return items;
+	}
+
+	public void setItems(List<ItemSolicitudTasadoDto> items) {
+		this.items = items;
 	}
 
 }
