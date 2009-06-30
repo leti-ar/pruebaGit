ac74d2e6b98208aac689dd4712623f5cea972c0b
jlgperez <jlgperez>
2009-06-30 15:44:47 +0000

    nuevas expresiones para email

--- src/main/java/ar/com/nextel/sfa/client/util/RegularExpressionConstants.java
+++ src/main/java/ar/com/nextel/sfa/client/util/RegularExpressionConstants.java
@@ -19,6 +19,12 @@ public class RegularExpressionConstants {
 	/** Numeros enteros infinito */
 	public static final String numeros = "[0-9]*";
 
+	/** Dirección de eMail */
+	public static final String email = "^(?:[a-zA-Z0-9_\'^&amp;/+-])+(?:\\.(?:[a-zA-Z0-9_\'^&amp;/+-])+)*@(?:(?:\\[?(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))\\.){3}(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\]?)|(?:[a-zA-Z0-9-]+\\.)+(?:[a-zA-Z]){2,}\\.?)$";
+	
+	/** Validacion sencilla para controlar email mientras se escribe */
+	public static final String lazyEmail = "(?:[\\.a-zA-Z0-9_\'^&amp;/+-])+)*@?(?:[\\.a-zA-Z0-9_\'^&amp;/+-])+)*";
+	
 	/** Permite ingresar una numeros hasta una cierta cantidad */
 	public static final String getNumerosLimitado(int cant) {
 		return "[0-9]{0," + cant + "}";
