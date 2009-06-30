bd66297f1c9f3a2e336ecfcb2ac35abae0832cd7
farambar <farambar>
2009-06-30 18:28:53 +0000

    new searchOpp

--- src/main/java/ar/com/nextel/sfa/server/OportunidadNegocioRpcServiceImpl.java
+++ src/main/java/ar/com/nextel/sfa/server/OportunidadNegocioRpcServiceImpl.java
@@ -8,21 +8,35 @@ import javax.servlet.ServletException;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
+import ar.com.nextel.business.dao.GenericDao;
 import ar.com.nextel.business.describable.GetAllBusinessOperator;
 import ar.com.nextel.business.oportunidades.search.SearchOportunidadBusinessOperator;
 import ar.com.nextel.business.oportunidades.search.businessUnits.OportunidadSearchData;
 import ar.com.nextel.business.oportunidades.search.result.OportunidadNegocioSearchResult;
+import ar.com.nextel.business.solicitudes.creation.SolicitudServicioBusinessOperator;
+import ar.com.nextel.business.solicitudes.search.dto.SolicitudServicioCerradaSearchCriteria;
 import ar.com.nextel.business.vendedores.RegistroVendedores;
-import ar.com.nextel.framework.security.Usuario;
+import ar.com.nextel.framework.repository.Repository;
 import ar.com.nextel.model.cuentas.beans.Vendedor;
+import ar.com.nextel.model.oportunidades.beans.EstadoOportunidad;
+import ar.com.nextel.model.oportunidades.beans.OperacionEnCurso;
+import ar.com.nextel.model.oportunidades.beans.OportunidadNegocio;
+import ar.com.nextel.model.oportunidades.beans.VentaPotencialVista;
+import ar.com.nextel.model.personas.beans.TipoDocumento;
+import ar.com.nextel.model.solicitudes.beans.EstadoSolicitud;
 import ar.com.nextel.services.components.sessionContext.SessionContextLoader;
 import ar.com.nextel.services.exceptions.BusinessException;
 import ar.com.nextel.sfa.client.OportunidadNegocioRpcService;
 import ar.com.nextel.sfa.client.dto.EstadoOportunidadDto;
-import ar.com.nextel.sfa.client.dto.OportunidadSearchDto;
+import ar.com.nextel.sfa.client.dto.EstadoSolicitudDto;
+import ar.com.nextel.sfa.client.dto.OportunidadDto;
+import ar.com.nextel.sfa.client.dto.OportunidadNegocioSearchResultDto;
 import ar.com.nextel.sfa.client.dto.TipoDocumentoDto;
+import ar.com.nextel.sfa.client.dto.VentasPotencialesYOperacionesEnCursoDto;
 import ar.com.nextel.sfa.client.initializer.BuscarOportunidadNegocioInitializer;
 import ar.com.nextel.sfa.server.util.MapperExtended;
+import ar.com.nextel.util.AppLogger;
+import ar.com.snoop.gwt.commons.client.exception.RpcExceptionMessages;
 import ar.com.snoop.gwt.commons.server.RemoteService;
 
 /**
@@ -32,29 +46,25 @@ public class OportunidadNegocioRpcServiceImpl extends RemoteService implements O
 
 	private WebApplicationContext context;
 	private RegistroVendedores registroVendedores;
-//	private Transformer transformer;
 	private SearchOportunidadBusinessOperator searchOportunidadBusinessOperator;
-	//private Transformer transformer;
 	private MapperExtended mapper;
-	//private Long totalCuentaRegs;
 	private GetAllBusinessOperator getAllBusinessOperator;
 	private SessionContextLoader sessionContextLoader;
+	private GenericDao genericDao;
+ 	private Repository repository;
 	
 	@Override
 	public void init() throws ServletException {
 		super.init();
-		context = WebApplicationContextUtils
-				.getWebApplicationContext(getServletContext());
+		context = WebApplicationContextUtils.getWebApplicationContext(getServletContext());
 		registroVendedores = (RegistroVendedores) context.getBean("registroVendedores");
-		searchOportunidadBusinessOperator = (SearchOportunidadBusinessOperator) context
-				.getBean("searchOportunidadBusinessOperatorBean");
-//		transformer = (Transformer) context
-//				.getBean("cuentaToSearchResultTransformer");
+		searchOportunidadBusinessOperator = (SearchOportunidadBusinessOperator) context.getBean("searchOportunidadBusinessOperatorBean");
 		sessionContextLoader = (SessionContextLoader) context.getBean("sessionContextLoader");
 		mapper = (MapperExtended) context.getBean("dozerMapper");
 		// Engancho el BOperator
-		setGetAllBusinessOperator((GetAllBusinessOperator) context
-				.getBean("getAllBusinessOperatorBean"));
+		setGetAllBusinessOperator((GetAllBusinessOperator) context.getBean("getAllBusinessOperatorBean"));
+		genericDao  = (GenericDao) context.getBean("genericDao");
+		repository = (Repository) context.getBean("repository");
 	}
 
 	public void setGetAllBusinessOperator(
@@ -62,44 +72,55 @@ public class OportunidadNegocioRpcServiceImpl extends RemoteService implements O
 		this.getAllBusinessOperator = getAllBusinessOperator;
 	}
 	
-	/**
-	 * @author eSalvador TODO: Quitar el HardCode cuando se logre obtener los
-	 *         datos reales de la Db.
-	 **/
 	public BuscarOportunidadNegocioInitializer getBuscarOportunidadInitializer() {
-		List<TipoDocumentoDto> listaTipoDoc = new ArrayList<TipoDocumentoDto>();
-		//listaTipoDoc.add(0, new TipoDocumentoDto("0", "Documento"));
-		//listaTipoDoc.add(1, new TipoDocumentoDto("1", "CUIT/CUIL"));
-
-		List<EstadoOportunidadDto> listaEstadoOpp = new ArrayList<EstadoOportunidadDto>();
-		listaEstadoOpp.add(0, new EstadoOportunidadDto("0", "Activa"));
-		listaEstadoOpp.add(1, new EstadoOportunidadDto("1", "Inactiva"));
-
-		BuscarOportunidadNegocioInitializer buscarDTOinit = new BuscarOportunidadNegocioInitializer(listaEstadoOpp, listaTipoDoc);
-		return buscarDTOinit;
-	}
-
-	public List<OportunidadSearchDto> searchOportunidad(OportunidadSearchDto oportunidadSearchDto)
-			 {
-		List dtoResult = new ArrayList();
-		OportunidadSearchData oppSearchData = (OportunidadSearchData) mapper.map(
-				oportunidadSearchDto, OportunidadSearchData.class);
-		
-		Vendedor vendedor = sessionContextLoader.getVendedor();
-		SessionContextLoader sessContext = (SessionContextLoader) context
-				.getBean("sessionContextLoader");
-		sessContext.getSessionContext().setVendedor(vendedor);
-		try {
-			List<OportunidadNegocioSearchResult> result = searchOportunidadBusinessOperator
-					.searchOportunidades(oppSearchData, vendedor,oportunidadSearchDto.getOffset(),
-							oportunidadSearchDto.getCantidadResultados());
-			for (OportunidadNegocioSearchResult oppNegocioSearchResult : result) {
-				dtoResult.add(mapper.map(oppNegocioSearchResult, OportunidadSearchDto.class));
-			}
-
-		} catch (BusinessException e) {
-			e.printStackTrace();
+		BuscarOportunidadNegocioInitializer buscarOportunidadNegocioInitializer = new BuscarOportunidadNegocioInitializer();
+		buscarOportunidadNegocioInitializer.setTiposDocumento(mapper.convertList(genericDao.getList(TipoDocumento.class), TipoDocumentoDto.class));
+		List<EstadoOportunidadDto> listaEstados = mapper.convertList(genericDao.getList(EstadoOportunidad.class), EstadoOportunidadDto.class);
+		while (listaEstados.size() > 2) {
+			listaEstados.remove(2);
 		}
-		return dtoResult;
+		buscarOportunidadNegocioInitializer.setEstadoOPP(listaEstados);
+		return buscarOportunidadNegocioInitializer;
 	}
+
+	public List<OportunidadNegocioSearchResultDto> searchOportunidad(OportunidadDto oportunidadDto) {
+		AppLogger.info("Iniciando busqueda de oportunidades...");
+		Vendedor vendedor = sessionContextLoader.getVendedor();
+		OportunidadSearchData oportunidadSearchData = mapper.map(oportunidadDto, OportunidadSearchData.class);		
+		List<OportunidadNegocioSearchResult> oportunidades = null;
+		try {		
+			oportunidades = this.searchOportunidadBusinessOperator.searchOportunidades(oportunidadSearchData, vendedor, oportunidadDto.getOffset(), oportunidadDto.getCantidadResultados());			
+		} catch (Exception e) {
+			AppLogger.info("Error buscando Oportunidades: " + e.getMessage());
+		}
+		List result = mapper.convertList(oportunidades, OportunidadNegocioSearchResultDto.class);
+		AppLogger.info("Busqueda de oportunidades finalizada.");
+		return result;
+		}
+
+//	public VentasPotencialesYOperacionesEnCursoDto ventasPotencialesYOperacionesEnCurso() throws RpcExceptionMessages {
+//		Vendedor vendedor = sessionContextLoader.getVendedor();
+//		return ventasPotencialesYOperacionesEnCurso(vendedor);
+//	}
+	
+//	private VentasPotencialesYOperacionesEnCursoDto ventasPotencialesYOperacionesEnCurso(Vendedor vendedor) {
+//		AppLogger.info("Obteniendo operaciones en curso para vendedor: " + vendedor.getUserName(), this);
+//		Set<VentaPotencialVista> ventasPotencialesEnCurso = vendedor.getVentasPotencialesVistaEnCurso();
+//		AppLogger.info("Son " + ventasPotencialesEnCurso.size() + " ventas potenciales");
+//
+//		Set<OperacionEnCurso> operacionesEnCurso = vendedor.getOperacionesEnCurso();
+//		AppLogger.info("Son " + operacionesEnCurso.size() + " operaciones en curso");
+//		//A partir de aca invente yo!
+//		VentasPotencialesYOperacionesEnCursoDto result = new VentasPotencialesYOperacionesEnCursoDto(ventasPotencialesEnCurso, operacionesEnCurso, vendedor.getCantidadCuentasPotencialesNoConsultadasActivasYVigentes());
+//		
+//
+////		VentasPotencialesYOperacionesEnCursoResult result = new VentasPotencialesYOperacionesEnCursoResult(ventasPotencialesEnCurso, operacionesEnCurso, vendedor
+////				.getCantidadCuentasPotencialesNoConsultadasActivasYVigentes());
+////		//mapeo VentasPotencialesYOperacionesEnCursoResult - VentasPotencialesYOperacionesEnCursoWCTO
+////		VentasPotencialesYOperacionesEnCursoWCTO resultWCTO = (VentasPotencialesYOperacionesEnCursoWCTO) 
+////		getSynchronizer().toWCTO(result);
+////		AppLogger.info("Obtencion de operaciones en curso finalizado.");
+//		return result;
+//	}
+
 }
