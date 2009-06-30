3666f53d87ccc86ba8525a4f5703e194eca77ac5
jlgperez <jlgperez>
2009-06-30 15:42:50 +0000

    nuevos metodos para saber de que tipo es. Se agregan Statics con los Ids

--- src/main/java/ar/com/nextel/sfa/client/dto/GrupoSolicitudDto.java
+++ src/main/java/ar/com/nextel/sfa/client/dto/GrupoSolicitudDto.java
@@ -1,38 +1,82 @@
 package ar.com.nextel.sfa.client.dto;
 
-import java.util.List;
-
 import com.google.gwt.user.client.rpc.IsSerializable;
 
-public class GrupoSolicitudDto implements IsSerializable{
+public class GrupoSolicitudDto implements IsSerializable {
 
-	private Long Id;
-    private String descripcion;
+	public static final Long ID_EQUIPOS_ACCESORIOS = Long.valueOf(1);
+	public static final Long ID_CDW = Long.valueOf(3);
+	public static final Long ID_MDS = Long.valueOf(4);
 
-    /**
-     * Los tipos de solicitud pertenecientes al grupo
-     */
-    private List<TipoSolicitudDto> tiposSolicitud;
-    
-    /** 
-     * Rangos para número de trípticos con/sin PIN
-     */
-    private Long rangoMinimoConPin;
-    private Long rangoMaximoConPin;
-    private Long rangoMinimoSinPin;
-    private Long rangoMaximoSinPin;
+	private Long id;
+	private String descripcion;
+
+	/**
+	 * Rangos para número de trípticos con/sin PIN
+	 */
+	private Long rangoMinimoConPin;
+	private Long rangoMaximoConPin;
+	private Long rangoMinimoSinPin;
+	private Long rangoMaximoSinPin;
+
 	public Long getId() {
-		return Id;
+		return id;
 	}
+
 	public void setId(Long id) {
-		Id = id;
+		this.id = id;
 	}
+
 	public String getDescripcion() {
 		return descripcion;
 	}
+
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
-    
-    
+
+	public Long getRangoMinimoConPin() {
+		return rangoMinimoConPin;
+	}
+
+	public void setRangoMinimoConPin(Long rangoMinimoConPin) {
+		this.rangoMinimoConPin = rangoMinimoConPin;
+	}
+
+	public Long getRangoMaximoConPin() {
+		return rangoMaximoConPin;
+	}
+
+	public void setRangoMaximoConPin(Long rangoMaximoConPin) {
+		this.rangoMaximoConPin = rangoMaximoConPin;
+	}
+
+	public Long getRangoMinimoSinPin() {
+		return rangoMinimoSinPin;
+	}
+
+	public void setRangoMinimoSinPin(Long rangoMinimoSinPin) {
+		this.rangoMinimoSinPin = rangoMinimoSinPin;
+	}
+
+	public Long getRangoMaximoSinPin() {
+		return rangoMaximoSinPin;
+	}
+
+	public void setRangoMaximoSinPin(Long rangoMaximoSinPin) {
+		this.rangoMaximoSinPin = rangoMaximoSinPin;
+	}
+
+	public boolean isEquiposAccesorios() {
+		return ID_EQUIPOS_ACCESORIOS.equals(id);
+	}
+
+	public boolean isCDW() {
+		return ID_CDW.equals(id);
+	}
+
+	public boolean isMDS() {
+		return ID_MDS.equals(id);
+	}
+
 }
