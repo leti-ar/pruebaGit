6e3ec3e621474459e3a2076388c316e88dd95264
farambar <farambar>
2009-07-06 19:52:22 +0000

    new

---
+++ src/main/java/ar/com/nextel/sfa/client/dto/VentaPotencialVistaDto.java
@@ -0,0 +1,86 @@
+package ar.com.nextel.sfa.client.dto;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+import ar.com.nextel.model.cuentas.beans.Vendedor;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+
+/**
+ * Clase: CuentaPotencialDto
+ * @author eSalvador
+ */
+
+public class VentaPotencialVistaDto	implements IsSerializable {
+
+    private Long idCuentaPotencial;
+    private boolean esReserva;
+    private String numeroCliente;
+    private String razonSocial;
+    private String telefono;
+    private String numero;
+    //private Vendedor vendedor;
+    private Boolean consultada;
+    private Date fechaAsignacion;
+	
+    
+    public Long getIdCuentaPotencial() {
+		return idCuentaPotencial;
+	}
+	public void setIdCuentaPotencial(Long idCuentaPotencial) {
+		this.idCuentaPotencial = idCuentaPotencial;
+	}
+	public boolean isEsReserva() {
+		return esReserva;
+	}
+	public void setEsReserva(boolean esReserva) {
+		this.esReserva = esReserva;
+	}
+	public String getNumeroCliente() {
+		return numeroCliente;
+	}
+	public void setNumeroCliente(String numeroCliente) {
+		this.numeroCliente = numeroCliente;
+	}
+	public String getRazonSocial() {
+		return razonSocial;
+	}
+	public void setRazonSocial(String razonSocial) {
+		this.razonSocial = razonSocial;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+//	public Vendedor getVendedor() {
+//		return vendedor;
+//	}
+//	public void setVendedor(Vendedor vendedor) {
+//		this.vendedor = vendedor;
+//	}
+	public Boolean getConsultada() {
+		return consultada;
+	}
+	public void setConsultada(Boolean consultada) {
+		this.consultada = consultada;
+	}
+	public Date getFechaAsignacion() {
+		return fechaAsignacion;
+	}
+	public void setFechaAsignacion(Date fechaAsignacion) {
+		this.fechaAsignacion = fechaAsignacion;
+	}
+    
+}
+
