8a7d913894b9977ae9b1e2512ca454a9db55c18c
jlgperez <jlgperez>
2009-07-14 21:10:06 +0000

    modifico clean

--- src/main/java/ar/com/nextel/sfa/client/util/FormUtils.java
+++ src/main/java/ar/com/nextel/sfa/client/util/FormUtils.java
@@ -6,6 +6,7 @@ import ar.com.nextel.sfa.client.widget.TelefonoTextBox;
 import ar.com.snoop.gwt.commons.client.widget.datepicker.DatePicker;
 
 import com.google.gwt.user.client.ui.CheckBox;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.TextBox;
@@ -31,16 +32,18 @@ public class FormUtils {
 				((CheckBox) field).setChecked(false);
 			} else if (field instanceof TelefonoTextBox) {
 				((TelefonoTextBox) field).clean();
-			} else if (field instanceof DatePicker){
+			} else if (field instanceof DatePicker) {
 				((DatePicker) field).resetSelectedDates();
 				((DatePicker) field).setSelectedDate(null);
+			} else if (field instanceof Label) {
+				((Label) field).setText("");
 			}
 		}
 	}
-	
+
 	/**
-	 * @author eSalvador
-	 * Metodo que ademas de limpiar los datos cargados, setea en Enable(true) los componentes.
+	 * @author eSalvador Metodo que ademas de limpiar los datos cargados, setea en Enable(true) los
+	 *         componentes.
 	 */
 	public static void cleanAndEnableFields(List<Widget> fields) {
 		for (Widget field : fields) {
@@ -56,17 +59,16 @@ public class FormUtils {
 				((CheckBox) field).setEnabled(true);
 			} else if (field instanceof TelefonoTextBox) {
 				((TelefonoTextBox) field).clean();
-			} else if (field instanceof DatePicker){
+			} else if (field instanceof DatePicker) {
 				((DatePicker) field).resetSelectedDates();
 				((DatePicker) field).setSelectedDate(null);
 				((DatePicker) field).setEnabled(true);
 			}
 		}
 	}
-	
+
 	/**
-	 * @author eSalvador
-	 * Metodo que SOLO setea en Enable(TRUE) los componentes.
+	 * @author eSalvador Metodo que SOLO setea en Enable(TRUE) los componentes.
 	 */
 	public static void enableFields(List<Widget> fields) {
 		for (Widget field : fields) {
@@ -77,16 +79,15 @@ public class FormUtils {
 			} else if (field instanceof CheckBox) {
 				((CheckBox) field).setEnabled(true);
 			} else if (field instanceof TelefonoTextBox) {
-				//TODO: Falta averiguar como se habilita/deshabilita.
-			} else if (field instanceof DatePicker){
+				// TODO: Falta averiguar como se habilita/deshabilita.
+			} else if (field instanceof DatePicker) {
 				((DatePicker) field).setEnabled(true);
 			}
 		}
 	}
-	
+
 	/**
-	 * @author eSalvador
-	 * Metodo que SOLO setea en Enable(FALSE) los componentes.
+	 * @author eSalvador Metodo que SOLO setea en Enable(FALSE) los componentes.
 	 */
 	public static void disableFields(List<Widget> fields) {
 		for (Widget field : fields) {
@@ -97,8 +98,8 @@ public class FormUtils {
 			} else if (field instanceof CheckBox) {
 				((CheckBox) field).setEnabled(false);
 			} else if (field instanceof TelefonoTextBox) {
-				//TODO: Falta averiguar como se habilita/deshabilita.
-			} else if (field instanceof DatePicker){
+				// TODO: Falta averiguar como se habilita/deshabilita.
+			} else if (field instanceof DatePicker) {
 				((DatePicker) field).setEnabled(false);
 			}
 		}
@@ -113,7 +114,7 @@ public class FormUtils {
 	public static boolean fieldDirty(Widget field, String dato) {
 		if (!field.isVisible())
 			return false;
-		if (dato==null) 
+		if (dato == null)
 			dato = "";
 		String fieldText = "";
 		if (field instanceof DatePicker) {
@@ -125,19 +126,19 @@ public class FormUtils {
 		} else if (field instanceof ListBox) {
 			fieldText = ((ListBox) field).getValue((((ListBox) field).getSelectedIndex()));
 		}
-		if (!fieldText.trim().equals(((String)dato).trim())) {
+		if (!fieldText.trim().equals(((String) dato).trim())) {
 			return true;
-		}	
+		}
 		return false;
-	}	
-	
+	}
+
 	/**
 	 * 
 	 * @param field
 	 * @return
 	 */
 	public static boolean fieldEmpty(Widget field) {
-		if (field==null)
+		if (field == null)
 			return true;
 		String fieldText = "";
 		if (field instanceof DatePicker) {
@@ -150,5 +151,5 @@ public class FormUtils {
 			fieldText = ((ListBox) field).getItemText((((ListBox) field).getSelectedIndex()));
 		}
 		return fieldText.trim().equals("");
-	}	
+	}
 }
