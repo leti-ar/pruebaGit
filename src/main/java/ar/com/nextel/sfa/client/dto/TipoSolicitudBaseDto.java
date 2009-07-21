040b053e08471dde583f899701c2c790d93ebd72
jlgperez <jlgperez>
2009-07-21 20:10:56 +0000

    almaceno Id de Solicitud Venta CDW

--- src/main/java/ar/com/nextel/sfa/client/dto/TipoSolicitudBaseDto.java
+++ src/main/java/ar/com/nextel/sfa/client/dto/TipoSolicitudBaseDto.java
@@ -4,6 +4,7 @@ import com.google.gwt.user.client.rpc.IsSerializable;
 
 public class TipoSolicitudBaseDto extends EnumDto implements IsSerializable {
 
+	public static final Long ID_VENTA_CDW = new Long(3);
 	private String formaContratacion;
 
 	public String getFormaContratacion() {
