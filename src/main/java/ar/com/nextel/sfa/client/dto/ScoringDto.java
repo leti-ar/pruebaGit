fcdc3808a0fa689c6bff218c904156bac09dd76a
mrial <mrial>
2009-08-04 19:11:26 +0000

    pop up de scoring para Infocom

--- src/main/java/ar/com/nextel/sfa/client/dto/ScoringDto.java
+++ src/main/java/ar/com/nextel/sfa/client/dto/ScoringDto.java
@@ -1,14 +1,52 @@
 package ar.com.nextel.sfa.client.dto;
 
-public class ScoringDto {
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+public class ScoringDto implements IsSerializable {
 	
-	private String scoring;
-
-	public String getScoring() {
-		return scoring;
+    private String scoringData;
+    private String cantidadCDW;
+    private String cantidadTerminales;
+    private String cantidadANIoTarjeta;
+    private String mensajeAdicional;
+    private String cantidadEquiposDDI;
+	
+    
+    public String getScoringData() {
+		return scoringData;
 	}
-
-	public void setScoring(String scoring) {
-		this.scoring = scoring;
+	public void setScoringData(String scoringData) {
+		this.scoringData = scoringData;
 	}
+	public String getCantidadCDW() {
+		return cantidadCDW;
+	}
+	public void setCantidadCDW(String cantidadCDW) {
+		this.cantidadCDW = cantidadCDW;
+	}
+	public String getCantidadTerminales() {
+		return cantidadTerminales;
+	}
+	public void setCantidadTerminales(String cantidadTerminales) {
+		this.cantidadTerminales = cantidadTerminales;
+	}
+	public String getCantidadANIoTarjeta() {
+		return cantidadANIoTarjeta;
+	}
+	public void setCantidadANIoTarjeta(String cantidadANIoTarjeta) {
+		this.cantidadANIoTarjeta = cantidadANIoTarjeta;
+	}
+	public String getMensajeAdicional() {
+		return mensajeAdicional;
+	}
+	public void setMensajeAdicional(String mensajeAdicional) {
+		this.mensajeAdicional = mensajeAdicional;
+	}
+	public String getCantidadEquiposDDI() {
+		return cantidadEquiposDDI;
+	}
+	public void setCantidadEquiposDDI(String cantidadEquiposDDI) {
+		this.cantidadEquiposDDI = cantidadEquiposDDI;
+	}   
+    
 }
--- src/main/java/ar/com/nextel/sfa/client/infocom/ScoringPopUpUI.java
+++ src/main/java/ar/com/nextel/sfa/client/infocom/ScoringPopUpUI.java
@@ -1,35 +1,53 @@
 package ar.com.nextel.sfa.client.infocom;
 
+import java.util.List;
+
+import ar.com.nextel.sfa.client.dto.EquipoDto;
+import ar.com.nextel.sfa.client.dto.ResumenEquipoDto;
 import ar.com.nextel.sfa.client.dto.ScoringDto;
 import ar.com.nextel.sfa.client.widget.NextelDialog;
 import ar.com.snoop.gwt.commons.client.widget.SimpleLink;
 
+import com.google.gwt.i18n.client.NumberFormat;
 import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.InlineHTML;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class ScoringPopUpUI extends NextelDialog {
 
-	public ScoringPopUpUI(String title,ScoringDto scoringDto) {
+	private ScoringDto scoring;
+	private FlexTable encabezadoTable;
+	
+	public ScoringPopUpUI(String title, ScoringDto scoring) {
 		super(title);
-		FlowPanel mainPanel = new FlowPanel();
-		mainPanel.setHeight("197px");
-		mainPanel.setWidth("380px");
-		mainPanel.add(new Label("Scoring:"));
-		Label scoringInfo = new Label();
-		scoringInfo.setText(scoringDto.getScoring());
-		mainPanel.add(scoringInfo);
-		this.add(mainPanel);
-		SimpleLink cerrar = new SimpleLink();
+		this.addStyleName("scoringDialog");
+		this.scoring = scoring;
+		initScoringPopUp();
+	}
+
+	private void initScoringPopUp() {
+		encabezadoTable = new FlexTable();
+		encabezadoTable.setWidget(0, 0, new Label("Scoring:"));
+		encabezadoTable.setWidget(1, 0, new Label(scoring.getMensajeAdicional()));
+		encabezadoTable.setWidth("380px");
+		encabezadoTable.setHeight("175px");
+		this.add(encabezadoTable);
+		SimpleLink cerrar = new SimpleLink("Cerrar");
 		cerrar.addStyleName("cerrarLink");
 		cerrar.addClickListener(new ClickListener() {
 			public void onClick(Widget arg0) {
 				ScoringPopUpUI.this.hide();
 			}
 		});
-		
+
 		this.addFooter(cerrar);
 	}
+
 }
