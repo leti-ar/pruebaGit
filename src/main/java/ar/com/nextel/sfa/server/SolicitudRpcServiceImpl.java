68febae058938432537a258cf505d69d464debcf
lpolupie <lpolupie>
2009-08-10 15:17:20 +0000

    Se modifico la validacion de la llamada al procedure generarChangelog

--- src/main/java/ar/com/nextel/sfa/server/SolicitudRpcServiceImpl.java
+++ src/main/java/ar/com/nextel/sfa/server/SolicitudRpcServiceImpl.java
@@ -457,15 +457,11 @@ public class SolicitudRpcServiceImpl extends RemoteService implements SolicitudR
 		try {
 			solicitudServicio = solicitudBusinessService.saveSolicitudServicio(solicitudServicioDto, mapper);
 			response = solicitudBusinessService.generarCerrarSolicitud(solicitudServicio, pinMaestro, cerrar);
-			// metodo changelog		
-			
-			if (cerrar == true && sessionContextLoader.getVendedor().getTipoVendedor().getCodigoVantive().equals(KnownInstanceIdentifier.TIPO_VENDEDOR_EECC.getKey())){
-				
-					generarChangeLog(solicitudServicioDto.getId(), solicitudServicio.getVendedor().getId());
-				
-			}
-		result.setError(response.getMessages().hasErrors());
-			result.setMessages(mapper.convertList(response.getMessages().getMessages(), MessageDto.class));
+			result.setError(response.getMessages().hasErrors());
+		if (cerrar == true && response.getMessages().hasErrors() == false && sessionContextLoader.getVendedor().getTipoVendedor().getCodigoVantive().equals(KnownInstanceIdentifier.TIPO_VENDEDOR_EECC.getKey())){
+			generarChangeLog(solicitudServicioDto.getId(), solicitudServicio.getVendedor().getId());
+		}
+		result.setMessages(mapper.convertList(response.getMessages().getMessages(), MessageDto.class));			
 		} catch (Exception e) {
 			AppLogger.error(e);
 			throw ExceptionUtil.wrap(e);
@@ -484,6 +480,7 @@ public class SolicitudRpcServiceImpl extends RemoteService implements SolicitudR
 			cstmt.setLong(1, idServicioDto);          	
 			cstmt.setLong(2, idVendedor);  		
 			cstmt.execute();
+
 		} catch (SQLException e) {          
 
 			e.printStackTrace();
