4380062af7f469852a3342ab16bdc33bb2437890
mrial <mrial>
2009-08-12 14:57:51 +0000

    se redefinieron algunos metodos

--- src/main/java/ar/com/nextel/sfa/client/cuenta/CuentaInfocomForm.java
+++ src/main/java/ar/com/nextel/sfa/client/cuenta/CuentaInfocomForm.java
@@ -1,51 +1,50 @@
 package ar.com.nextel.sfa.client.cuenta;
 
 import ar.com.nextel.sfa.client.infocom.InfocomUI;
+import ar.com.nextel.sfa.client.util.HistoryUtils;
 
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.FlexTable;
 
 /**
- * @author eSalvador 
+ * @author eSalvador
  **/
-public class CuentaInfocomForm  extends Composite {
+public class CuentaInfocomForm extends Composite {
 
-	private FlexTable mainPanel = new FlexTable();
 	private InfocomUI infocomUI;
 	private static CuentaInfocomForm instance = new CuentaInfocomForm();
-	
-	
+	private String idCuenta;
+
 	public static CuentaInfocomForm getInstance() {
+		if (instance == null) {
+			instance = new CuentaInfocomForm();
+		}
 		return instance;
 	}
-	
-	public CuentaInfocomForm() {
+
+	private CuentaInfocomForm() {
 		init();
 	}
-	
+
 	private void init() {
-		infocomUI = new InfocomUI();
-		infocomUI.firstLoad();
-//		infocomUI = new InfocomUI();
-		initWidget(mainPanel);
-		mainPanel.setWidth("100%");
-//		mainPanel.setWidget(0, 0, infocomUI.firstLoad());
-//		mainPanel.add(creditoFidelizacionPanel);
-//		mainPanel.add(cuentaCorrientePanel);
+		//infocomUI = new InfocomUI();
+		infocomUI = InfocomUI.getInstance();
+		Long cuentaID = Long.parseLong(HistoryUtils.getParam("cuenta_id"));
+		infocomUI.setIdCuenta(cuentaID.toString());
+		infocomUI.getInstance().firstLoad();
+		initWidget(infocomUI);
 	}
-	
-//	private void createDatosInfocom() {
-//		creditoFidelizacionPanel = new FidelizacionInfocomUI();
-//		cuentaCorrientePanel = new CCInfocomUI();
-//	}
-//	
-//	public void setFidelizacionPanel(FidelizacionInfocomUI fidelizacionInfocomUI) {
-//		this.creditoFidelizacionPanel = fidelizacionInfocomUI;
-//
-//	}
-//	
-//	public void setCCPanel(CCInfocomUI ccInfocomUI) {
-//		this.cuentaCorrientePanel = ccInfocomUI;
-//
-//	}
+
+	public void load() {
+		InfocomUI.getInstance().setIdCuenta(idCuenta);
+		InfocomUI.getInstance().load();
+	}
+
+	public String getIdCuenta() {
+		return idCuenta;
+	}
+
+	public void setIdCuenta(String idCuenta) {
+		this.idCuenta = idCuenta;
+	}
+
 }
