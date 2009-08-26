6b5055e1797fb22150a331e8b8aef038df585e77
rgrippo <rgrippo>
2009-08-26 18:31:26 +0000

    agrego descripcion

--- src/main/java/ar/com/nextel/sfa/client/enums/SexoEnum.java
+++ src/main/java/ar/com/nextel/sfa/client/enums/SexoEnum.java
@@ -1,21 +1,24 @@
 package ar.com.nextel.sfa.client.enums;
 
 public enum SexoEnum {
-	INDEFINIDO   (1), 
-	FEMENINO     (2),
-	MASCULINO    (3),
-	ORGANIZACION (4);
+	INDEFINIDO   (1,"Indefinido"), 
+	FEMENINO     (2,"Femenino"),
+	MASCULINO    (3,"Masculino"),
+	ORGANIZACION (4,"Organización");
 	
 	private long id;
-	
-	SexoEnum(long id) {
+	private String descripcion;
+	SexoEnum(long id,String desc) {
 		this.id=id;
+		this.descripcion=desc;
 	}
 	
 	public long getId() {
 		return id;
 	}
-	
+	public String getDescripcion() {
+		return descripcion;
+	}
 	public static long getId(String nroDoc) {
 		return (nroDoc.startsWith("30") || nroDoc.startsWith("33")) ? SexoEnum.ORGANIZACION.getId() : SexoEnum.MASCULINO.getId();
 	}
