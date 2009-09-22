8c8516000bd895d546aaa3867d58af5469f354e4
mrial <mrial>
2009-09-22 16:29:36 +0000

    Bug Fixed 20305. Se agregó el botón Cancelar a la pantalla de Infocom.

--- src/main/java/ar/com/nextel/sfa/client/infocom/VerInfocomUI.java
+++ src/main/java/ar/com/nextel/sfa/client/infocom/VerInfocomUI.java
@@ -1,15 +1,29 @@
 package ar.com.nextel.sfa.client.infocom;
 
-import ar.com.nextel.sfa.client.widget.ApplicationUI;
-import ar.com.nextel.sfa.client.widget.RazonSocialClienteBar;
+import java.util.Date;
 
+import ar.com.nextel.sfa.client.constant.Sfa;
+import ar.com.nextel.sfa.client.widget.ApplicationUI;
+import ar.com.nextel.sfa.client.widget.FormButtonsBar;
+import ar.com.nextel.sfa.client.widget.MessageDialog;
+import ar.com.nextel.sfa.client.widget.RazonSocialClienteBar;
+import ar.com.snoop.gwt.commons.client.util.DateUtil;
+import ar.com.snoop.gwt.commons.client.widget.SimpleLink;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.DeferredCommand;
+import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.IncrementalCommand;
+import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.Widget;
 
 public class VerInfocomUI extends ApplicationUI {
 
 	private long cuentaID;
 	private static VerInfocomUI instance = new VerInfocomUI();
+	private FormButtonsBar footerBar;
+	private SimpleLink cancelarLink;
 	InfocomUI infocomUI;
 	RazonSocialClienteBar razonSocialClienteBar = new RazonSocialClienteBar();
 	
@@ -33,8 +47,19 @@ public class VerInfocomUI extends ApplicationUI {
 		mainPanel.add(razonSocialClienteBar);
 		razonSocialClienteBar.setDisabledSilvioSoldan();
 		infocomUI.addStyleName("fondoGris");
+		cancelarLink = new SimpleLink(Sfa.constant().cancelar(), "#", true);
+		cancelarLink.addClickListener(new ClickListener() {
+			public void onClick (Widget arg0) {
+				cancelar();			
+			}
+		});
 	}
 
+	private void cancelar() {
+		MessageDialog.getInstance().hide();
+		History.newItem("");
+	}
+	
 	public boolean load() {
 		infocomUI.loadApplication();
 		DeferredCommand.addCommand(new IncrementalCommand() {
@@ -49,9 +74,15 @@ public class VerInfocomUI extends ApplicationUI {
 			}
 		});
 		mainPanel.add(infocomUI);
+		mainPanel.add(getFooter());
 		return true;
 	}
-
+	
+	public FormButtonsBar getFooter() {
+		footerBar = new FormButtonsBar();
+		footerBar.addLink(cancelarLink);
+		return footerBar;
+	}
 
 	@Override
 	public boolean unload(String token) {
@@ -68,6 +99,7 @@ public class VerInfocomUI extends ApplicationUI {
 	public void setCuentaID(long cuentaID) {
 		this.cuentaID = cuentaID;
 	}
+
 
 	
 }
