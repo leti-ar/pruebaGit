47eea866e0537ed8b8653b357ceae51854588d58
mrial <mrial>
2009-09-23 14:36:57 +0000

    new

---
+++ src/main/java/ar/com/nextel/sfa/client/ss/ExportarExcelSSResultUI.java
@@ -0,0 +1,38 @@
+package ar.com.nextel.sfa.client.ss;
+
+import ar.com.nextel.sfa.client.image.IconFactory;
+
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.InlineHTML;
+
+public class ExportarExcelSSResultUI extends FlowPanel {
+
+	private HTML icon;
+	private InlineHTML numResultados = new InlineHTML("Número de Resultados: ");
+	
+	public ExportarExcelSSResultUI() {
+		this.setHeight("30px");
+		icon = IconFactory.excel();
+		icon.addStyleName("exportarExcelSS mr10");
+		numResultados.setWidth("170px");
+		numResultados.addStyleName("floatLeft");
+		numResultados.addStyleName("mt5");
+		add(numResultados);
+		add(icon);
+	}
+	
+	public InlineHTML getNumResultados() {
+		return numResultados;
+	}
+	public void setNumResultados(String numResultados) {
+		this.numResultados.setText(numResultados);
+	}
+	public HTML getIcon() {
+		return icon;
+	}
+	public void setIcon(HTML icon) {
+		this.icon = icon;
+	}
+					
+}
