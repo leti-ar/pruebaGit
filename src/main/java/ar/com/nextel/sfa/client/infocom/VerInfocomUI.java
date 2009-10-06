9e0e92edc37155106d4b49e488488ff543be7132
rgrippo <rgrippo>
2009-10-06 19:47:37 +0000

    fix para que el metodo getFooter() devuelva siempre una unica instancia del  FromButtonBar

--- src/main/java/ar/com/nextel/sfa/client/infocom/VerInfocomUI.java
+++ src/main/java/ar/com/nextel/sfa/client/infocom/VerInfocomUI.java
@@ -1,17 +1,12 @@
 package ar.com.nextel.sfa.client.infocom;
 
-import java.util.Date;
-
 import ar.com.nextel.sfa.client.constant.Sfa;
 import ar.com.nextel.sfa.client.widget.ApplicationUI;
 import ar.com.nextel.sfa.client.widget.FormButtonsBar;
 import ar.com.nextel.sfa.client.widget.MessageDialog;
 import ar.com.nextel.sfa.client.widget.RazonSocialClienteBar;
-import ar.com.snoop.gwt.commons.client.util.DateUtil;
 import ar.com.snoop.gwt.commons.client.widget.SimpleLink;
 
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.DeferredCommand;
 import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.IncrementalCommand;
@@ -25,7 +20,7 @@ public class VerInfocomUI extends ApplicationUI {
 	private FormButtonsBar footerBar;
 	private SimpleLink cancelarLink;
 	InfocomUI infocomUI;
-	RazonSocialClienteBar razonSocialClienteBar = new RazonSocialClienteBar();
+	RazonSocialClienteBar razonSocialClienteBar;
 	
 	
 	public static VerInfocomUI getInstance() {
@@ -42,6 +37,8 @@ public class VerInfocomUI extends ApplicationUI {
 
 
 	public void firstLoad() {
+		razonSocialClienteBar = new RazonSocialClienteBar();
+		footerBar = new FormButtonsBar();
 		infocomUI = InfocomUI.getInstance();
 		//Agrega la barra de razon social y cliente
 		mainPanel.add(razonSocialClienteBar);
@@ -53,6 +50,7 @@ public class VerInfocomUI extends ApplicationUI {
 				cancelar();			
 			}
 		});
+		footerBar.addLink(cancelarLink);
 	}
 
 	private void cancelar() {
@@ -79,8 +77,6 @@ public class VerInfocomUI extends ApplicationUI {
 	}
 	
 	public FormButtonsBar getFooter() {
-		footerBar = new FormButtonsBar();
-		footerBar.addLink(cancelarLink);
 		return footerBar;
 	}
 
