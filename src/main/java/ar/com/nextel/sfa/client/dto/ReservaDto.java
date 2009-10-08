56e4ed0fdb10f5acbaafa4fee758f05542ddaa3c
mrial <mrial>
2009-10-08 17:40:35 +0000

    new

---
+++ src/main/java/ar/com/nextel/sfa/client/dto/ReservaDto.java
@@ -0,0 +1,8 @@
+package ar.com.nextel.sfa.client.dto;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+public class ReservaDto extends CuentaPotencialDto implements IsSerializable {
+	
+
+}
