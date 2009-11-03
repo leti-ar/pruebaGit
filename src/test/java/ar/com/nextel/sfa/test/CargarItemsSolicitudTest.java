a57b2b2b45bce1b35295eef9d7a9fffa144c2091
jlgperez <jlgperez>
2009-11-03 13:39:28 +0000

    cambios para que funcione con la nueva version

--- src/test/java/ar/com/nextel/sfa/test/CargarItemsSolicitudTest.java
+++ src/test/java/ar/com/nextel/sfa/test/CargarItemsSolicitudTest.java
@@ -7,7 +7,7 @@ import ar.com.nextel.sfa.client.debug.DebugConstants;
  * 
  * 
  * @author jlgperez
- *
+ * 
  */
 public class CargarItemsSolicitudTest extends SfaSeleniumTest {
 
@@ -24,24 +24,32 @@ public class CargarItemsSolicitudTest extends SfaSeleniumTest {
 		waitForElement(DebugConstants.GWT_MENU_CUENTAS_BUSCAR);
 		selenium.click(DebugConstants.GWT_MENU_CUENTAS_BUSCAR);
 
-		waitForElement("//select[@id='"
-				+ DebugConstants.GWT_BUSQUEDA_CUENTAS_COMBO_PREDEFINIDAS + "']/option[@value='1']");
-		selenium.focus(DebugConstants.GWT_BUSQUEDA_CUENTAS_COMBO_PREDEFINIDAS);
-		selenium.select(DebugConstants.GWT_BUSQUEDA_CUENTAS_COMBO_PREDEFINIDAS, "label=Ctas. propias");
-		selenium.focus(DebugConstants.GWT_BUSQUEDA_CUENTAS_BOTON_BUSCAR);
-		selenium.click(DebugConstants.GWT_BUSQUEDA_CUENTAS_BOTON_BUSCAR);
-
 		for (int i = 0; i < 10; i++) {
+			waitForElement("//select[@id='" + DebugConstants.GWT_BUSQUEDA_CUENTAS_COMBO_PREDEFINIDAS
+					+ "']/option[@value='1']");
+			selenium.focus(DebugConstants.GWT_BUSQUEDA_CUENTAS_COMBO_PREDEFINIDAS);
+			selenium.select(DebugConstants.GWT_BUSQUEDA_CUENTAS_COMBO_PREDEFINIDAS, "label=Ctas. propias");
+			selenium.focus(DebugConstants.GWT_BUSQUEDA_CUENTAS_BOTON_BUSCAR);
+			selenium.click(DebugConstants.GWT_BUSQUEDA_CUENTAS_BOTON_BUSCAR);
+
 			cargarItemsSolicituParaUnaCuenta(i + 1);
+			
+			waitForElement(DebugConstants.GWT_MENU_CUENTAS);
+			selenium.click(DebugConstants.GWT_MENU_CUENTAS);
+			waitForElement(DebugConstants.GWT_MENU_CUENTAS_BUSCAR);
+
+			selenium.click(DebugConstants.GWT_MENU_CUENTAS_BUSCAR);
+			Thread.sleep(500);
+			selenium.click("link=no");
 		}
 		assertTrue(true);
 	}
 
 	private void cargarItemsSolicituParaUnaCuenta(int row) throws Exception {
 
-		waitForElement("//table[@id='"
-				+ DebugConstants.GWT_BUSQUEDA_CUENTAS_TABLE_RESULT + "']/tbody/tr[4]/td[5]");
-		
+		waitForElement("//table[@id='" + DebugConstants.GWT_BUSQUEDA_CUENTAS_TABLE_RESULT
+				+ "']/tbody/tr[4]/td[5]");
+
 		selenium.mouseOver("//table[@id='" + DebugConstants.GWT_BUSQUEDA_CUENTAS_TABLE_RESULT
 				+ "']/tbody/tr[" + (row + 1) + "]/td[5]");
 		selenium.click("//table[@id='" + DebugConstants.GWT_BUSQUEDA_CUENTAS_TABLE_RESULT + "']/tbody/tr["
@@ -49,7 +57,6 @@ public class CargarItemsSolicitudTest extends SfaSeleniumTest {
 		selenium.click("link=^Crear SS");
 
 		waitForElement("link=Equipos/Accesorios");
-		
 
 		selenium.click("link=Equipos/Accesorios");
 
@@ -62,10 +69,11 @@ public class CargarItemsSolicitudTest extends SfaSeleniumTest {
 				return;
 			}
 		} catch (Exception e) {
+			return;
 		}
-
+		Thread.sleep(500);
 		waitForElement(DebugConstants.GWT_EDITAR_SOLICITUD_DATOS_BUTTON_CREAR_LINEA);
-		
+
 		waitWhileCargando();
 		selenium.click(DebugConstants.GWT_EDITAR_SOLICITUD_DATOS_BUTTON_CREAR_LINEA);
 		waitForElement(DebugConstants.GWT_EDITAR_SOLICITUD_ITEM_SOLICITUD_COMBO_ITEM);
@@ -77,49 +85,58 @@ public class CargarItemsSolicitudTest extends SfaSeleniumTest {
 				"label=Kit i880 Motorola");
 		selenium.type(DebugConstants.GWT_EDITAR_SOLICITUD_ITEM_SOLICITUD_TEXTBOX_CANTIDAD, "2");
 		waitWhileCargando();
+		selenium.select(DebugConstants.GWT_EDITAR_SOLICITUD_ITEM_SOLICITUD_COMBO_PLAN, "label=Directo 100");
+		waitWhileCargando();
 		Thread.sleep(500);
 		selenium.click("link=ACEPTAR");
 		waitWhileCargando();
-		selenium.click("//div[3]/table/tbody/tr[3]/td[3]");
+		Thread.sleep(500);
+		selenium.click("//table[@id='" + DebugConstants.GWT_EDITAR_SOLICITUD_DATOS_DETTALE_TABLE
+				+ "']/tbody/tr[3]/td[3]");
 		waitWhileCargando();
 		selenium.click(DebugConstants.GWT_EDITAR_SOLICITUD_DATOS_BUTTON_CREAR_LINEA);
-		
+
 		waitForElement(DebugConstants.GWT_EDITAR_SOLICITUD_ITEM_SOLICITUD_COMBO_ITEM);
-		
+
 		// Creo una venta
 
 		selenium.select(DebugConstants.GWT_EDITAR_SOLICITUD_ITEM_SOLICITUD_COMBO_TIPO_ORDEN,
 				"label=Venta (AMBA)");
 		waitWhileCargando();
-		selenium.select(DebugConstants.GWT_EDITAR_SOLICITUD_ITEM_SOLICITUD_COMBO_ITEM,
-				"label=Kit i570 Motorola");
+		selenium.select(DebugConstants.GWT_EDITAR_SOLICITUD_ITEM_SOLICITUD_COMBO_ITEM, "index=1");
+		waitWhileCargando();
+		selenium.select(DebugConstants.GWT_EDITAR_SOLICITUD_ITEM_SOLICITUD_COMBO_PLAN, "index=1");
+		Thread.sleep(500);
 		selenium.type(DebugConstants.GWT_EDITAR_SOLICITUD_ITEM_SOLICITUD_TEXTBOX_CANTIDAD, "1");
 		Thread.sleep(500);
 		selenium.click("link=ACEPTAR");
 		waitWhileCargando();
-		selenium.click("//div[3]/table/tbody/tr[4]/td[3]");
+		Thread.sleep(500);
+		selenium.click("//table[@id='" + DebugConstants.GWT_EDITAR_SOLICITUD_DATOS_DETTALE_TABLE
+				+ "']/tbody/tr[4]/td[3]");
 		waitWhileCargando();
 		selenium.click(DebugConstants.GWT_EDITAR_SOLICITUD_DATOS_BUTTON_CREAR_LINEA);
-		
+
 		waitForElement(DebugConstants.GWT_EDITAR_SOLICITUD_ITEM_SOLICITUD_COMBO_ITEM);
-		
+
 		// Creo venta de Accesorios
 
 		selenium.select(DebugConstants.GWT_EDITAR_SOLICITUD_ITEM_SOLICITUD_COMBO_TIPO_ORDEN,
 				"label=Venta de Accesorios");
 		waitWhileCargando();
-		selenium.select(DebugConstants.GWT_EDITAR_SOLICITUD_ITEM_SOLICITUD_COMBO_ITEM,
-				"label=NTN2306 - Tapa i833");
+		selenium.select(DebugConstants.GWT_EDITAR_SOLICITUD_ITEM_SOLICITUD_COMBO_ITEM, "index=1");
 		selenium.type(DebugConstants.GWT_EDITAR_SOLICITUD_ITEM_SOLICITUD_TEXTBOX_CANTIDAD, "1");
 		Thread.sleep(500);
 		selenium.click("link=ACEPTAR");
 		waitWhileCargando();
-		selenium.click("//div[3]/table/tbody/tr[5]/td[3]");
+		Thread.sleep(500);
+		selenium.click("//table[@id='" + DebugConstants.GWT_EDITAR_SOLICITUD_DATOS_DETTALE_TABLE
+				+ "']/tbody/tr[5]/td[3]");
 		waitWhileCargando();
 		selenium.click(DebugConstants.GWT_EDITAR_SOLICITUD_DATOS_BUTTON_CREAR_LINEA);
-		
+
 		waitForElement(DebugConstants.GWT_EDITAR_SOLICITUD_ITEM_SOLICITUD_COMBO_ITEM);
-		
+
 		// Creo Activacion
 
 		selenium.select(DebugConstants.GWT_EDITAR_SOLICITUD_ITEM_SOLICITUD_COMBO_TIPO_ORDEN,
@@ -128,16 +145,19 @@ public class CargarItemsSolicitudTest extends SfaSeleniumTest {
 		selenium.type(DebugConstants.GWT_EDITAR_SOLICITUD_ITEM_SOLICITUD_TEXTBOX_IMEI, "123456789012345");
 		selenium.click("//table[2]/tbody/tr[1]/td[3]/div/div");
 		waitWhileCargando();
-		selenium.select(DebugConstants.GWT_EDITAR_SOLICITUD_ITEM_SOLICITUD_COMBO_MODELO,
-				"label=Equipo BB7520 RIM");
+		selenium.select(DebugConstants.GWT_EDITAR_SOLICITUD_ITEM_SOLICITUD_COMBO_MODELO, "index=1");
+		selenium.select(DebugConstants.GWT_EDITAR_SOLICITUD_ITEM_SOLICITUD_COMBO_ITEM, "index=1");
+		waitWhileCargando();
 		selenium.type(DebugConstants.GWT_EDITAR_SOLICITUD_ITEM_SOLICITUD_TEXTBOX_SIM, "123456789012345");
 		waitWhileCargando();
 		selenium.click("//td[5]/div/div");
 		selenium.type(DebugConstants.GWT_EDITAR_SOLICITUD_ITEM_SOLICITUD_TEXTBOX_PIN, "12345678");
-		selenium.select(DebugConstants.GWT_EDITAR_SOLICITUD_ITEM_SOLICITUD_COMBO_TIPO_PLAN,
-				"label=Plan No Vigente");
 		waitWhileCargando();
-		selenium.select(DebugConstants.GWT_EDITAR_SOLICITUD_ITEM_SOLICITUD_COMBO_PLAN, "label=NEXTEL 400");
+		try {
+			selenium.select(DebugConstants.GWT_EDITAR_SOLICITUD_ITEM_SOLICITUD_COMBO_PLAN, "index=1");
+		} catch (Exception e) {
+			selenium.select(DebugConstants.GWT_EDITAR_SOLICITUD_ITEM_SOLICITUD_COMBO_PLAN, "index=0");
+		}
 		Thread.sleep(500);
 		selenium.click("link=ACEPTAR");
 		waitWhileCargando();
@@ -148,21 +168,25 @@ public class CargarItemsSolicitudTest extends SfaSeleniumTest {
 		selenium.select(DebugConstants.GWT_EDITAR_SOLICITUD_ITEM_SOLICITUD_COMBO_TIPO_ORDEN,
 				"label=Venta Licencias BB");
 		waitWhileCargando();
+		selenium.select(DebugConstants.GWT_EDITAR_SOLICITUD_ITEM_SOLICITUD_COMBO_ITEM, "index=1");
+		waitWhileCargando();
 		selenium.type(DebugConstants.GWT_EDITAR_SOLICITUD_ITEM_SOLICITUD_TEXTBOX_CANTIDAD, "2");
 		Thread.sleep(500);
 		selenium.click("link=ACEPTAR");
 		waitWhileCargando();
-		selenium.click("//div[3]/table/tbody/tr[6]/td[3]");
-		selenium.click("//tr[7]/td[3]");
-		selenium.click("//button[@type='button']");
+		Thread.sleep(500);
+		selenium.click("//table[@id='" + DebugConstants.GWT_EDITAR_SOLICITUD_DATOS_DETTALE_TABLE
+				+ "']/tbody/tr[6]/td[3]");
+		waitWhileCargando();
+//		selenium.click("//button[@type='button']");
 
 		// Vuelvo a la pantalla de busqueda
-		waitForElement(DebugConstants.GWT_MENU_CUENTAS);
-		selenium.click(DebugConstants.GWT_MENU_CUENTAS);
-		waitForElement(DebugConstants.GWT_MENU_CUENTAS_BUSCAR);
-
-		selenium.click(DebugConstants.GWT_MENU_CUENTAS_BUSCAR);
-		Thread.sleep(500);
-		selenium.click("link=no");
+//		waitForElement(DebugConstants.GWT_MENU_CUENTAS);
+//		selenium.click(DebugConstants.GWT_MENU_CUENTAS);
+//		waitForElement(DebugConstants.GWT_MENU_CUENTAS_BUSCAR);
+//
+//		selenium.click(DebugConstants.GWT_MENU_CUENTAS_BUSCAR);
+//		Thread.sleep(500);
+//		selenium.click("link=no");
 	}
 }
