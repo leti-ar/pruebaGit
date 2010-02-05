b1783c505107d24438f5eaba42606a16b77a32e6
jlgperez <jlgperez>
2010-02-05 14:45:14 +0000

    Se agregan ComentarioDto a la CuentaPotencialDto para mostrar las notas

---
+++ src/main/java/ar/com/nextel/sfa/client/dto/ComentarioDto.java
@@ -0,0 +1,52 @@
+package ar.com.nextel.sfa.client.dto;
+
+import java.util.Date;
+
+public class ComentarioDto {
+	private Date fechaAlta;
+	private String usuario;
+	private String textoComentario;
+	private String titulo;
+	private String areaOrigen;
+
+	public Date getFechaAlta() {
+		return fechaAlta;
+	}
+
+	public void setFechaAlta(Date fechaAlta) {
+		this.fechaAlta = fechaAlta;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getTextoComentario() {
+		return textoComentario;
+	}
+
+	public void setTextoComentario(String textoComentario) {
+		this.textoComentario = textoComentario;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAreaOrigen() {
+		return areaOrigen;
+	}
+
+	public void setAreaOrigen(String areaOrigen) {
+		this.areaOrigen = areaOrigen;
+	}
+
+}
--- src/main/java/ar/com/nextel/sfa/client/dto/CuentaPotencialDto.java
+++ src/main/java/ar/com/nextel/sfa/client/dto/CuentaPotencialDto.java
@@ -1,118 +1,155 @@
 package ar.com.nextel.sfa.client.dto;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 import com.google.gwt.user.client.rpc.IsSerializable;
 
-
 public class CuentaPotencialDto implements IsSerializable {
 	private Long id;
-    private String codigoVantive;
-    private String numero;
-    private Date fechaVencimiento;
-    private Date fechaAsignacion;
-    private CuentaDto cuentaOrigen;
-    private Short terminalesEstimadas;
-    private String diaHorarioVisitas;
-    private PersonaDto persona;
-    private RubroDto rubro;
-    private VendedorDto vendedor;
-//    private List<ComentarioCuentaPotencial> comentarios = new HashSet<ComentarioCuentaPotencial>();
-//    private List<ContactoCuentaPotencial> contactos = new HashSet<ContactoCuentaPotencial>();
-    private Boolean vencimientoProcesado = Boolean.FALSE;
-    private Boolean vencimientoLockOportunidad = Boolean.FALSE;
-    private Boolean consultada = Boolean.FALSE;
-    protected boolean esReserva;
+	private String codigoVantive;
+	private String numero;
+	private Date fechaVencimiento;
+	private Date fechaAsignacion;
+	private CuentaDto cuentaOrigen;
+	private Short terminalesEstimadas;
+	private String diaHorarioVisitas;
+	private PersonaDto persona;
+	private RubroDto rubro;
+	private VendedorDto vendedor;
+	private ArrayList<ComentarioDto> comentarios = new ArrayList<ComentarioDto>();
+	// private List<ContactoCuentaPotencial> contactos = new HashSet<ContactoCuentaPotencial>();
+	private Boolean vencimientoProcesado = Boolean.FALSE;
+	private Boolean vencimientoLockOportunidad = Boolean.FALSE;
+	private Boolean consultada = Boolean.FALSE;
+	protected boolean esReserva;
 
-
-    public Long getId() {
+	public Long getId() {
 		return id;
 	}
-    public void setId(Long id) {
+
+	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getCodigoVantive() {
 		return codigoVantive;
 	}
+
 	public void setCodigoVantive(String codigoVantive) {
 		this.codigoVantive = codigoVantive;
 	}
+
 	public String getNumero() {
 		return numero;
 	}
+
 	public void setNumero(String numero) {
 		this.numero = numero;
 	}
+
 	public Date getFechaVencimiento() {
 		return fechaVencimiento;
 	}
+
 	public void setFechaVencimiento(Date fechaVencimiento) {
 		this.fechaVencimiento = fechaVencimiento;
 	}
+
 	public Date getFechaAsignacion() {
 		return fechaAsignacion;
 	}
+
 	public void setFechaAsignacion(Date fechaAsignacion) {
 		this.fechaAsignacion = fechaAsignacion;
 	}
+
 	public CuentaDto getCuentaOrigen() {
 		return cuentaOrigen;
 	}
+
 	public void setCuentaOrigen(CuentaDto cuentaOrigen) {
 		this.cuentaOrigen = cuentaOrigen;
 	}
+
 	public Short getTerminalesEstimadas() {
 		return terminalesEstimadas;
 	}
+
 	public void setTerminalesEstimadas(Short terminalesEstimadas) {
 		this.terminalesEstimadas = terminalesEstimadas;
 	}
+
 	public String getDiaHorarioVisitas() {
 		return diaHorarioVisitas;
 	}
+
 	public void setDiaHorarioVisitas(String diaHorarioVisitas) {
 		this.diaHorarioVisitas = diaHorarioVisitas;
 	}
+
 	public PersonaDto getPersona() {
 		return persona;
 	}
+
 	public void setPersona(PersonaDto persona) {
 		this.persona = persona;
 	}
+
 	public RubroDto getRubro() {
 		return rubro;
 	}
+
 	public void setRubro(RubroDto rubro) {
 		this.rubro = rubro;
 	}
+
 	public VendedorDto getVendedor() {
 		return vendedor;
 	}
+
 	public void setVendedor(VendedorDto vendedor) {
 		this.vendedor = vendedor;
 	}
+
 	public Boolean getVencimientoProcesado() {
 		return vencimientoProcesado;
 	}
+
 	public void setVencimientoProcesado(Boolean vencimientoProcesado) {
 		this.vencimientoProcesado = vencimientoProcesado;
 	}
+
 	public Boolean getVencimientoLockOportunidad() {
 		return vencimientoLockOportunidad;
 	}
+
 	public void setVencimientoLockOportunidad(Boolean vencimientoLockOportunidad) {
 		this.vencimientoLockOportunidad = vencimientoLockOportunidad;
 	}
+
 	public Boolean getConsultada() {
 		return consultada;
 	}
+
 	public void setConsultada(Boolean consultada) {
 		this.consultada = consultada;
 	}
+
 	public boolean isEsReserva() {
 		return esReserva;
 	}
+
 	public void setEsReserva(boolean esReserva) {
 		this.esReserva = esReserva;
 	}
+
+	public ArrayList<ComentarioDto> getComentarios() {
+		return comentarios;
+	}
+
+	public void setComentarios(ArrayList<ComentarioDto> comentarios) {
+		this.comentarios = comentarios;
+	}
+
 }
