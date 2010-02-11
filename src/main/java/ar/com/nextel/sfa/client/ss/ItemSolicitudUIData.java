c7509501468049515295825320031ae9f18227b1
jlgperez <jlgperez>
2010-02-11 21:02:08 +0000

    Incomplete - # 442: Desreserva de numero de telefono http://bugtracker.nextel.com.ar/view.php?id=442

--- src/main/java/ar/com/nextel/sfa/client/ss/ItemSolicitudUIData.java
+++ src/main/java/ar/com/nextel/sfa/client/ss/ItemSolicitudUIData.java
@@ -938,7 +938,7 @@ public class ItemSolicitudUIData extends UIData implements ChangeListener, Click
 	public void desreservarSiNoFueGrabado() {
 		// Si no fue guardado nunca no tiene tipo
 		if (lineaSolicitudServicio.getTipoSolicitud() == null) {
-			String numeroReservado = lineaSolicitudServicio.getNumeroReserva();
+			String numeroReservado = getNumeroTelefonicoCompletoFromFields();
 			boolean tieneNReserva = numeroReservado != null && numeroReservado.length() > 4;
 			if (tieneNReserva) {
 				controller.desreservarNumeroTelefonico(Long.parseLong(numeroReservado),
