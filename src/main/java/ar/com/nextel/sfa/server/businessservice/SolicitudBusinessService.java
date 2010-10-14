31b9b15f28e59bdb58fd8869f82fcba6443d1017
dgdubisk <dgdubisk>
2010-10-14 20:39:52 +0000

    # 1079 merge desde el head inicio: TAG_TELEMARKETING_BR fin: sfa-pom-2_0_14

--- src/main/java/ar/com/nextel/sfa/server/businessservice/SolicitudBusinessService.java
+++ src/main/java/ar/com/nextel/sfa/server/businessservice/SolicitudBusinessService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import ar.com.nextel.business.cuentas.facturaelectronica.FacturaElectronicaService;
 import ar.com.nextel.business.legacy.financial.FinancialSystem;
 import ar.com.nextel.business.legacy.financial.dto.EncabezadoCreditoDTO;
 import ar.com.nextel.business.legacy.financial.exception.FinancialSystemException;
@@ -64,6 +65,12 @@ public class SolicitudBusinessService {
 	private final String CUENTA_FILTRADA = "Acceso denegado. No puede operar con esta cuenta.";
 	private TransactionConnectionDAO sfaConnectionDAO;
 	private GenerarChangelogConfig generarChangelogConfig;
+	private FacturaElectronicaService facturaElectronicaService;
+
+	@Autowired
+	public void setFacturaElectronicaService(FacturaElectronicaService facturaElectronicaService) {
+		this.facturaElectronicaService = facturaElectronicaService;
+	}
 
 	@Autowired
 	public void setSolicitudesBusinessOperator(
@@ -293,7 +300,7 @@ public class SolicitudBusinessService {
 	@Transactional(readOnly = false, propagation = Propagation.REQUIRES_NEW)
 	public GeneracionCierreResponse generarCerrarSolicitud(SolicitudServicio solicitudServicio,
 			String pinMaestro, boolean cerrar) {
-
+		boolean esProspect = solicitudServicio.getCuenta().isEnCarga();
 		if (!GenericValidator.isBlankOrNull(pinMaestro) && solicitudServicio.getCuenta().isEnCarga()) {
 			solicitudServicio.getCuenta().setPinMaestro(pinMaestro);
 		} else {
@@ -308,10 +315,30 @@ public class SolicitudBusinessService {
 		GeneracionCierreResponse response = null;
 		if (cerrar) {
 			response = generacionCierreBusinessOperator.cerrarSolicitudServicio(generacionCierreRequest);
+
+			AppLogger.error("IF replicacion a autogestion, FE: "
+					+ solicitudServicio.getCuenta().getFacturaElectronica() + " tiene errores: "
+					+ response.getMessages().hasErrors(), this);
+
+			if (solicitudServicio.getCuenta().getFacturaElectronica() != null
+					&& !solicitudServicio.getCuenta().getFacturaElectronica().getReplicadaAutogestion()
+					&& !response.getMessages().hasErrors()) {
+				if (!esProspect) {
+					facturaElectronicaService.adherirFacturaElectronica(
+							solicitudServicio.getCuenta().getId(), solicitudServicio.getCuenta()
+									.getCodigoVantive(), solicitudServicio.getCuenta()
+									.getFacturaElectronica().getEmail(), "", solicitudServicio.getVendedor()
+									.getUserName());
+				}
+				solicitudServicio.getCuenta().getFacturaElectronica().setReplicadaAutogestion(Boolean.TRUE);
+				repository.save(solicitudServicio.getCuenta().getFacturaElectronica());
+			}
 		} else {
 			response = generacionCierreBusinessOperator.generarSolicitudServicio(generacionCierreRequest);
 		}
+
 		repository.save(solicitudServicio);
+
 		return response;
 	}
 
