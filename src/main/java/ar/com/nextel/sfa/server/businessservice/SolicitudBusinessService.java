d9cb5278e42fa46e894075d38060a7b8283f449c
learce <learce>
2011-02-08 15:24:42 +0000

    se agregó una condición para que sólo a los clientes se les cree una gestión al cerrar una SS.

--- src/main/java/ar/com/nextel/sfa/server/businessservice/SolicitudBusinessService.java
+++ src/main/java/ar/com/nextel/sfa/server/businessservice/SolicitudBusinessService.java
@@ -335,18 +335,18 @@ public class SolicitudBusinessService {
 									.getCodigoVantive(), solicitudServicio.getCuenta()
 									.getFacturaElectronica().getEmail(), "", solicitudServicio.getVendedor()
 									.getUserName());
+					solicitudServicio.getCuenta().getFacturaElectronica().setReplicadaAutogestion(Boolean.TRUE);
+					repository.save(solicitudServicio.getCuenta().getFacturaElectronica());
+					String codigoGestion = "TRANSF_FACT_ELECTRONICA";
+					ParametrosGestion parametrosGestion = repository.retrieve(ParametrosGestion.class, codigoGestion);
+					generacionCierreBusinessOperator.lanzarGestionCerrarSS(
+							this.sessionContextLoader.getInstance().getVendedor().getUserName(), 
+							0L, 
+							parametrosGestion, 
+							"", 
+							solicitudServicio.getCuenta().getId(), 
+							cerrar);
 				}
-				solicitudServicio.getCuenta().getFacturaElectronica().setReplicadaAutogestion(Boolean.TRUE);
-				repository.save(solicitudServicio.getCuenta().getFacturaElectronica());
-				String codigoGestion = "TRANSF_FACT_ELECTRONICA";
-				ParametrosGestion parametrosGestion = repository.retrieve(ParametrosGestion.class, codigoGestion);
-				generacionCierreBusinessOperator.lanzarGestionCerrarSS(
-						this.sessionContextLoader.getInstance().getVendedor().getUserName(), 
-						0L, 
-						parametrosGestion, 
-						"", 
-						solicitudServicio.getCuenta().getId(), 
-						cerrar);
 			}
 		} else {
 			response = generacionCierreBusinessOperator.generarSolicitudServicio(generacionCierreRequest);
