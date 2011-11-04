9e049bbde0ccb5cb2e2e23b90a3de7802e577e21
learce <learce>
2011-11-04 15:17:09 +0000

    Merge con SFA_SS_TRANSF_BR

--- src/main/java/ar/com/nextel/sfa/client/ss/BusqClienteCedenteDialog.java
+++ src/main/java/ar/com/nextel/sfa/client/ss/BusqClienteCedenteDialog.java
@@ -54,6 +54,7 @@ public class BusqClienteCedenteDialog extends NextelDialog implements ChangeHand
 
 	public void setBuscarCommand(Command command){
 		buscarCommand = command;
+		busqClienteCedenteUI.setBuscarCommand(command); //#1422
 	}
 	
 	public BusqClienteCedenteUIData getBusqClienteCedenteUIData() {
