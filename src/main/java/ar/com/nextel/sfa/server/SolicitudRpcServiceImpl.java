0a559c25b06b04828cac3c50182edbfcac708fc5
ewatem <ewatem>
2012-09-03 18:10:54 +0000

    MERGE CON FIX SOLUCIONADO DE PRODUCCION

--- src/main/java/ar/com/nextel/sfa/server/SolicitudRpcServiceImpl.java
+++ src/main/java/ar/com/nextel/sfa/server/SolicitudRpcServiceImpl.java
@@ -550,8 +550,9 @@ public class SolicitudRpcServiceImpl extends RemoteService implements SolicitudR
 					TipoDocumentoDto.class));
 		}
 
-		mapper.convertList(repository.getAll(EstadoPorSolicitud.class),
-				EstadoPorSolicitudDto.class);
+//		MGR - Se comenta esto ya que aparentemente no hace nada y trae problemas
+//		mapper.convertList(repository.getAll(EstadoPorSolicitud.class),
+//				EstadoPorSolicitudDto.class);
 				
 		return buscarSSCerradasInitializer;
 	}
@@ -2101,7 +2102,7 @@ public boolean saveEstadoPorSolicitudDto(EstadoPorSolicitudDto estadoPorSolicitu
 					return false;
 				}
 			} else {// En caso de no estar activa la consulta al veraz, se deben evaluar 
-					// las condiciones comerciales para un resultado de veraz “REVISAR”
+					// las condiciones comerciales para un resultado de veraz REVISARA
 				resultadoVerazScoring = "REVISAR";
 			}
 			
